b21c45e09a3d32a1021cf16431511d5bec4193e9
lenacoll <lena.collienne@gmail.com>
2020-08-30 13:12:36 +1200

    trying to make FP run on BIG trees: int -> long, function to compute distance without saving moves

--- tree.c
+++ tree.c
@@ -9,14 +9,14 @@ basic algorithms for ranked trees*/
 
 // trees will be arrays of nodes, ordered according to their ranks (first n nodes are leaves, order there doesn't matter)
 typedef struct Node{
-    int parent;
-    int children[2];
+    long parent;
+    long children[2];
 } Node;
 
 
 // List of trees (e.g. as output of NNI move (2 trees) or findpath(d trees))
 typedef struct Tree_List{
-    int num_leaves;
+    long num_leaves;
     int num_trees;
     Node** trees;
 } Tree_List;
@@ -38,81 +38,87 @@ int get_num_digits(int integer){
 Tree_List read_trees_from_file(char* filename){
     FILE *f;
     if ((f = fopen(filename, "r"))){
-        int num_leaves;
+        long num_leaves;
         int num_trees;
-        char * buffer0 = malloc(20 * sizeof(char)); // max number of digits for number of leaves and number of trees in file is assumed to be 20
+        char * first_ints_buffer = malloc(10 * sizeof(char)); // max number of digits for number of leaves and number of trees in file is assumed to be 20
         // read in first two lines: number of leaves and number of trees
-        if (fgets(buffer0, 10 * sizeof(char), f) != NULL){
-            num_leaves = atoi(buffer0);
+        if (fgets(first_ints_buffer, 10 * sizeof(char), f) != NULL){
+            num_leaves = atoi(first_ints_buffer);
             if (num_leaves == 0){
                 printf("Error. Determine number of leaves in input file.\n");
             }
         } // TODO: add else to give error when input file does not have the right format
-        if (fgets(buffer0, 10 * sizeof(char), f) != NULL){
-            num_trees = atoi(buffer0);
+        if (fgets(first_ints_buffer, 10 * sizeof(char), f) != NULL){
+            num_trees = atoi(first_ints_buffer);
             if (num_trees == 0){
                 printf("Error. Determine number of trees in input file.\n");
             }
         }
-        free(buffer0);
-        int num_nodes = num_leaves*2 - 1;
+        free(first_ints_buffer);
+
+        long num_nodes = num_leaves*2 - 1;
         int num_digits_n = get_num_digits(num_leaves); // number of digits of the int num_leaves
-        int max_str_length = 2 * num_leaves * num_leaves * num_digits_n; //upper bound for the maximum length of a tree as string
-        
+        long max_str_length = 2 * num_leaves * num_leaves * num_digits_n; //upper bound for the maximum length of a tree as string -- this is quite a bad approximation and should be improved (?)        
         Tree_List tree_list;
         tree_list.num_leaves = num_leaves;
         tree_list.num_trees = num_trees;
         tree_list.trees = malloc(num_trees*sizeof(Node*));
         for (int i = 0; i < num_trees; i++){
             tree_list.trees[i] = malloc(num_nodes * sizeof(Node));
-            for (int j = 0; j < num_nodes; j++){
+            for (long j = 0; j < num_nodes; j++){
                 tree_list.trees[i][j].parent = -1;
                 tree_list.trees[i][j].children[0] = -1;
                 tree_list.trees[i][j].children[1] = -1;
             }
         }
 
-        int *highest_ancestor = malloc(num_leaves * sizeof(int)); // highest_ancestor[i]: index of cluster containing leaf i that is highest below the currently considered cluster
-        for(int i = 0; i < num_leaves; i++){
+        long *highest_ancestor = malloc(num_leaves * sizeof(long)); // highest_ancestor[i]: index of cluster containing leaf i that is highest below the currently considered cluster
+        for(long i = 0; i < num_leaves; i++){
             highest_ancestor[i] = 1;
         }
+        printf("max str len: %ld\n", max_str_length);
         char *buffer = malloc(max_str_length * sizeof(char*));
-        for(int i = 0; i < max_str_length; i++){
+        for(long i = 0; i < max_str_length; i++){
             buffer[i] = '\0';
         }
-        int current_tree = 0;
+        printf("Start reading first tree\n");
+        long current_tree = 0;
         //loop through lines (trees) in file
         while(fgets(buffer, max_str_length * sizeof(char), f) != NULL){
             // Remove white spaces from string buffer
-            int l = 0;
-            for(int k=0;buffer[k]!='\0';++k)
+            long l = 0;
+            for(long k=0;buffer[k]!='\0';++k)
             {
                 if(buffer[k]!=' ')
                     buffer[l++]=buffer[k];
             }
             buffer[l]='\0';
-
             // allocate memory for strings saving clusters
             char *tree_str = malloc(max_str_length * sizeof(char));
-            char *cluster;
-            for(int i = 0; i < max_str_length; i++){
+            for(long i = 0; i < max_str_length; i++){
                 tree_str[i] = buffer[i];
                 if(buffer[i] == ']'){
                     break;
                 }
             }
             tree_str[strcspn(tree_str, "\n")] = 0; // delete newline at end of each line that has been read
-            int rank = num_leaves;
+            long rank = num_leaves;
             //Find clusters
             char * partial_tree_str = tree_str;
             char * cluster_list;
+            printf("tree: %s\n", partial_tree_str);
             while((cluster_list = strsep(&partial_tree_str, "}")) != NULL){
                 cluster_list += 2; // ignore first two characters [{ or ,{
                 if(strlen(cluster_list) > 0){ //ignore last bit (just contained ])
+                    printf("cluster list: %s\n", cluster_list);
                     // Find leaves in clusters
+                    char * cluster;
                     while((cluster = strsep(&cluster_list, ",")) != NULL){
-                        int actual_leaf = atoi(cluster);
+                        long actual_leaf = atol(cluster);
+                        printf("actual leaf: %ld\n", actual_leaf);
                         // update node relations if current leaf appears for first time
+                        printf("current tree: %ld, actual leaf: %ld ", current_tree, actual_leaf);
+                        printf("parent: %ld\n", tree_list.trees[current_tree][actual_leaf - 1].parent);
                         if(tree_list.trees[current_tree][actual_leaf - 1].parent == -1){
                             tree_list.trees[current_tree][actual_leaf - 1].parent = rank;
                             // update the current internal node (rank) to have the current leaf as child
@@ -138,7 +144,6 @@ Tree_List read_trees_from_file(char* filename){
                 rank++;
             }
             current_tree++;
-            free(cluster_list);
             free(tree_str);
         }
         free(buffer);
@@ -323,7 +328,7 @@ void write_tree(Node * tree, int num_leaves, char * filename){
 void write_trees(Tree_List tree_list, char * filename){
     FILE *f;
     f = fopen(filename, "w");
-    fprintf(f, "%d\n", tree_list.num_leaves);
+    fprintf(f, "%ld\n", tree_list.num_leaves);
     fprintf(f, "%d\n", tree_list.num_trees);
     fclose(f);
     for (int i = 0; i < tree_list.num_trees; i++){
@@ -333,13 +338,13 @@ void write_trees(Tree_List tree_list, char * filename){
 
 
 // NNI move on edge bounded by rank rank_in_list and rank_in_list + 1, moving child_stays (index) of the lower node up
-int nni_move(Node * tree, int rank_in_list, int num_leaves, int child_moves_up){
+int nni_move(Node * tree, long rank_in_list, long num_leaves, int child_moves_up){
     if (tree == NULL){
         printf("Error. No RNNI move possible. Given tree doesn't exist.\n");
     } else{
-        int num_nodes = 2 * num_leaves - 1;
+        long num_nodes = 2 * num_leaves - 1;
         if(tree[rank_in_list].parent != rank_in_list + 1){
-            printf("Can't do an NNI - interval [%d, %d] is not an edge!\n", rank_in_list, rank_in_list + 1);
+            printf("Can't do an NNI - interval [%ld, %ld] is not an edge!\n", rank_in_list, rank_in_list + 1);
             return 1;
         } else{
             int child_moved_up;
@@ -360,24 +365,24 @@ int nni_move(Node * tree, int rank_in_list, int num_leaves, int child_moves_up){
 
 
 // Make a rank move on tree between nodes of rank rank and rank + 1 (if possible)
-int rank_move(Node * tree, int rank_in_list, int num_leaves){
-    int num_nodes = 2 * num_leaves - 1;
+int rank_move(Node * tree, long rank_in_list, long num_leaves){
+    long num_nodes = 2 * num_leaves - 1;
     if (tree == NULL){
         printf("Error. No rank move possible. Given tree doesn't exist.\n");
         return 1;
     } else{
         if (tree[rank_in_list].parent == rank_in_list + 1){
-            printf("Error. No rank move possible. The interval [%d,%d] is an edge!\n", rank_in_list, rank_in_list + 1);
+            printf("Error. No rank move possible. The interval [%ld,%ld] is an edge!\n", rank_in_list, rank_in_list + 1);
         } else{
             // update parents of nodes that swap ranks
-            int upper_parent;
+            long upper_parent;
             upper_parent = tree[rank_in_list + 1].parent;
             tree[rank_in_list + 1].parent = tree[rank_in_list].parent;
             tree[rank_in_list].parent = upper_parent;
 
             for (int i = 0; i < 2; i++){
                 // update children of nodes that swap ranks
-                int upper_child = tree[rank_in_list + 1].children[i];
+                long upper_child = tree[rank_in_list + 1].children[i];
                 tree[rank_in_list + 1].children[i] = tree[rank_in_list].children[i];
                 tree[rank_in_list].children[i] = upper_child;
             }
@@ -408,9 +413,9 @@ int rank_move(Node * tree, int rank_in_list, int num_leaves){
 
 
 // find mrca of nodes with positions node1 and node2 in tree
-int mrca(Node * tree, int node1, int node2){
-    int rank1 = node1;
-    int rank2 = node2;
+long mrca(Node * tree, long node1, long node2){
+    long rank1 = node1;
+    long rank2 = node2;
     while (rank1 != rank2){
         if (rank1 < rank2){
             rank1 = tree[rank1].parent;
@@ -492,6 +497,80 @@ int ** findpath(Node *start_tree, Node *dest_tree, int num_leaves){
     }
     moves[0][0] = path_index - 1;
     return moves;
+}
+
+
+// returns a path in matrix representation -- explanation in data_structures.md
+long findpath_distance(Node *start_tree, Node *dest_tree, long num_leaves){
+    long max_dist = ((num_leaves - 1) * (num_leaves - 2))/2 + 1;
+    // int ** moves = malloc((max_dist + 1) * sizeof(int*)); // save moves in a table: each row (after the first) is move, column 1: rank of lower node bounding the interval of move, column 2: 0,1,2: rank move, nni where children[0] stays, nni where children[1] stays; the first row only contains distance between the trees (moves[0][0])
+    // for (int i = 0; i < max_dist + 1; i++){
+    //     moves[i] = malloc(2 * sizeof(int));
+    //     moves[i][0] = 0;
+    //     moves[i][1] = 0;
+    // }
+    long path_index = 1; // next position on path that we want to fill with a tree pointer
+    if (start_tree == NULL){
+        printf("Error. Start tree doesn't exist.\n");
+    } else if (dest_tree == NULL){
+        printf("Error. Destination tree doesn't exist.\n");
+    } else{
+        remove("./output/findpath.rtree");
+        // write_tree(start_tree, num_leaves, "./output/findpath.rtree"); // this ruins the running time!!!!!!!!
+        long current_mrca; //rank of the mrca that needs to be moved down
+        Node * current_tree = malloc((2 * num_leaves - 1) * sizeof(Node));
+        for (long i = 0; i < 2 * num_leaves - 1; i++){
+            current_tree[i] = start_tree[i];
+        }
+
+        for (long i = num_leaves; i < 2 * num_leaves - 1; i++){
+            current_mrca = mrca(current_tree, dest_tree[i].children[0], dest_tree[i].children[1]);
+            // move current_mrca down
+            while(current_mrca != i){
+                bool did_nni = false;
+                for (int child_index = 0; child_index < 2; child_index++){ // find out if one of the children of current_tree[current_mrca] has rank current_mrca - 1. If this is the case, we want to make an NNI
+                    if (did_nni == false && current_tree[current_mrca].children[child_index] == current_mrca - 1){ // do nni if current interval is an edge
+                        // check which of the children of current_tree[current_mrca] should move up by the NNI move 
+                        bool found_child = false; //indicate if we found the correct child
+                        int child_stays; // index of the child of current_tree[current_mrca] that does not move up by an NNI move
+                        // find the index of the child of the parent of the node we currently consider -- this will be the index child_stays that we want in the end
+                        long current_child_index = dest_tree[i].children[0]; // rank of already existing cluster in both current_tree and dest_tree
+                        while (found_child == false){
+                            while (current_tree[current_child_index].parent < current_mrca - 1){ // find the x for which dest_tree[i].children[x] is contained in the cluster induced by current_tree[current_mrca - 1]
+                                current_child_index = current_tree[current_child_index].parent;
+                            }
+                            // find the index child_stays
+                            if(current_tree[current_child_index].parent == current_mrca - 1){
+                                found_child = true;
+                                if (current_tree[current_tree[current_child_index].parent].children[0] == current_child_index){
+                                    child_stays = 0;
+                                } else{
+                                    child_stays = 1;
+                                }
+                            } else{
+                                current_child_index = dest_tree[i].children[1];
+                            }
+                        }
+                        nni_move(current_tree, current_mrca - 1, num_leaves, 1 - child_stays);
+                        // moves[path_index][1] = 1 + child_stays;
+                        did_nni = true;
+                        current_mrca--;
+                    }
+                }
+                if (did_nni == false){
+                    rank_move(current_tree, current_mrca - 1, num_leaves);
+                    // moves[path_index][1] = 0;
+                    current_mrca--;
+                }
+                // moves[path_index][0] = current_mrca;
+                path_index++;
+            }
+        }
+        free(current_tree);
+    }
+    // moves[0][0] = path_index - 1;
+    // return moves;
+    return path_index - 1;
 }
 
 
@@ -581,10 +660,12 @@ int main(){
     printf("What is the file containing trees?\n");
     scanf("%s", filename);
     
+    printf("Start reading trees from file\n");
     Tree_List tree_list = read_trees_from_file(filename);
-    int num_trees = tree_list.num_trees;
-    int num_leaves = tree_list.num_leaves;
-    int num_nodes = 2 * num_leaves - 1;
+    printf("End reading trees from file\n");
+    long num_trees = tree_list.num_trees;
+    long num_leaves = tree_list.num_leaves;
+    long num_nodes = 2 * num_leaves - 1;
 
     // // // check if read_trees_from_file reads trees correctly
     // // for (int k = 0; k < num_trees; k++){
@@ -604,21 +685,24 @@ int main(){
     // write_trees(tree_list, "./output/output.rtree"); // write given trees into file
     // Tree_List findpath_list = return_findpath(tree_list); // write FP into file
     clock_t start_time = time(NULL);
-    int ** fp = findpath(tree_list.trees[0], tree_list.trees[1], tree_list.num_leaves); //run FP
+    printf("Start running FindPath\n");
+    // int ** fp = findpath(tree_list.trees[0], tree_list.trees[1], tree_list.num_leaves); //run FP
+    long distance = findpath_distance(tree_list.trees[0], tree_list.trees[1], tree_list.num_leaves);
     clock_t end_time = time(NULL);
-    int max_dist = ((num_leaves - 1) * (num_leaves - 2))/2 + 1;
-    int distance = fp[0][0];
-    for (int i = 0; i < max_dist + 1; i++){
-        free(fp[i]);
-    }
-    free(fp);
+    printf("End running FindPath\n");
+    long max_dist = ((num_leaves - 1) * (num_leaves - 2))/2 + 1;
+    // int distance = fp[0][0];
+    // for (int i = 0; i < max_dist + 1; i++){
+    //     free(fp[i]);
+    // }
+    // free(fp);
 
     free(tree_list.trees[0]);
     free(tree_list.trees[1]);
     free(tree_list.trees);
 
     printf("Time to compute FP(T,R): %f sec\n", difftime(end_time, start_time));
-    printf("Length of fp: %d\n", distance);
+    printf("Length of fp: %ld\n", distance);
     // write_trees(findpath_list, "./output/fp.rtree");
     // printf("distance: %d\n", distance(5, "[{1,2},{1,2,3},{1,2,3,4},{1,2,3,4,5}]", "[{4,5},{3,4,5},{1,2},{1,2,3,4,5}]"));
     return 0;
