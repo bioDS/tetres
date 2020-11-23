b232d325e9c2532ce0425d3c637f4147d5054d87
lenacoll <lena.collienne@gmail.com>
2020-11-23 17:37:30 +1300

    fix ranking of internal node when reading trees

--- src/tree_io.py
+++ src/tree_io.py
@@ -21,30 +21,26 @@ def read_newick(s):
     while (len(tree_str) > 0): # recurse over tree and replace internal nodes with leaves labelled by internal_nodei for int i and save information about internal node height and children
 
         if int_node_index < num_int_nodes - 1: # as long as we don't reach the root
-            pattern = r'\((\w+):((\d+.\d+)|(\d+)),(\w+):((\d+.\d+)|(\d+))\):((\d+.\d+)|(\d+))'
+            pattern = r'\((\w+):(\[[^\]]*\])?((\d+.\d+)|(\d+)),(\w+):(\[[^\]]*\])?((\d+.\d+)|(\d+))\):(\[[^\]]*\])?((\d+.\d+)|(\d+))'
         else: # we reach the root -- string of form '(node1:x,node2:y)' left
-            pattern = r'\((\w+):((\d+.\d+)|(\d+)),(\w+):((\d+.\d+)|(\d+))\)'
+            pattern = r'\((\w+):(\[[^\]]*\])?((\d+.\d+)|(\d+)),(\w+):(\[[^\]]*\])?((\d+.\d+)|(\d+))\);?'
 
         int_node_str = re.search(pattern, tree_str)
-        
+
         # Save new internal node as parent of its two children
         parent_dict[int_node_str.group(1)] = int_node_index
-        parent_dict[int_node_str.group(5)] = int_node_index
+        parent_dict[int_node_str.group(6)] = int_node_index
 
-        if 'internal_node' in int_node_str.group(1):
-            child_index = int(re.search(r'internal_node(\d*)', int_node_str.group(1)).group(1)) # index of child, so that height of new internal node is height of child + length of edge between child and parent
-            int_node_height[int_node_index] = int_node_height[child_index] + float(int_node_str.group(2))
-        else:
-            int_node_height[int_node_index] = float(int_node_str.group(2)) # added at index int_node_index
+        int_node_height[int_node_index] = float(int_node_str.group(3)) # add height of new internal node to int_node_index   
 
-        if int_node_index < num_int_nodes - 1: # insert new leaf 'internal_nodei' for a pair of leaves
-            repl = "internal_node" + str(int_node_index) + ":" + str(float(int_node_str.group(9)))
+        if int_node_index < num_int_nodes - 1: # insert new leaf 'internal_nodei' replacing the found pattern (pair of leaves)
+            repl = "internal_node" + str(int_node_index) + ":" + str(float(int_node_height[int_node_index])+ float(int_node_str.group(12)))
             tree_str = re.sub(pattern, repl, tree_str, count = 1)
             int_node_index += 1
         else: # If we consider root, replace tree_str with empty str
             tree_str = ''
 
-    # Now we use parent_lust and int_node_height to create tree in C TREE data structure
+    # Now we use parent_list and int_node_height to create tree in C TREE data structure
     node_list = (NODE * num_nodes)()
     node_name_list = [] # Save the names of leaves in a list, such that entry i corresponds to leaf at position i in node list of tree structure
 
@@ -53,20 +49,21 @@ def read_newick(s):
         node_list[i].parent = -1
         node_list[i].children[0] = -1
         node_list[i].children[1] = -1
+        node_list[i].name = ''
         node_name_list.append('')
 
     # sort internal nodes according to rank/times (increasing) -- output: list of keys (indices of internal nodes)
     int_node_list = [key for key, val in sorted(int_node_height.items(), key=lambda item:item[1])]
 
     leaf_index = 0 # Index for leaves, they are ordered alphabetical! (To make sure that two trees on same leaf set have same mapping of position in NODE array to label)
-    parent_dict = dict(sorted(parent_dict.items(), key=lambda item: item[0])) # sort parent_dict alphabetical according to node names (internal_nodex + leaf labels) to make sure leaf labels are added to NODE listin alphabetical order
+    parent_dict = dict(sorted(parent_dict.items(), key=lambda item: item[0])) # sort parent_dict alphabetical according to node names (internal_nodex + leaf labels) to make sure leaf labels are added to NODE list in alphabetical order
 
     for node in parent_dict:
         int_node_str = re.search(r'internal_node(\d*)', node)
         if int_node_str != None: # Consider internal node
             child_rank = num_leaves + int_node_list.index(int(int_node_str.group(1)))
             parent_rank = num_leaves + int_node_list.index(int(parent_dict[int_node_str.group()]))
-            node_name_list[parent_rank] = node
+            node_name_list[parent_rank] = str(node)
             # Set parent
             node_list[child_rank].parent = parent_rank
             # Set children (only one, make sure to use the slot children[0] ot children[1] that is not used already)
@@ -76,7 +73,7 @@ def read_newick(s):
                 node_list[parent_rank].children[1] = child_rank
         else: # Consider leaf
             parent_rank = num_leaves + int_node_list.index(int(parent_dict[node]))
-            node_name_list[leaf_index] = node
+            node_list[leaf_index].name = str(node)
             # Set parent
             node_list[leaf_index].parent = parent_rank
             # Set children (only one, make sure to use the slot children[0] ot children[1] that is not used already)
@@ -84,7 +81,8 @@ def read_newick(s):
                 node_list[parent_rank].children[0] = leaf_index
             else:
                 node_list[parent_rank].children[1] = leaf_index
+            node_name_list[leaf_index] = str(node)
             leaf_index += 1
-    
+
     output_tree = TREE(num_leaves, node_list)
     return(output_tree)
