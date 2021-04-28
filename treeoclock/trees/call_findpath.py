c0e040e26187726e0f5311bdf5e90d513c3338bc
Lars-B <berlinglars96@gmail.com>
2021-04-28 08:41:43 +0200

    Function for findpath, deleted init for structs

--- treeoclock/trees/call_findpath.py
+++ treeoclock/trees/call_findpath.py
@@ -1,9 +1,7 @@
 __author__ = 'Lena Collienne and Jordan Kettles'
 
 import os
-from ctypes import *
-
-from numpy.ctypeslib import ndpointer
+from ctypes import *  # TODO
 
 lib = CDLL(f'{os.path.dirname(os.path.realpath(__file__))}/findpath.so')
 
@@ -12,28 +10,38 @@ class NODE(Structure):
     _fields_ = [('parent', c_long), ('children', c_long * 2),
                 ('time', c_long)]  # The order of arguments here matters! Needs to be the same as in C code!
 
-    def __init_(self, parent, children, time):
-        self.parent = parent
-        self.children = children
-        self.time = time
+    # def __init_(self, parent, children, time):
+    #     self.parent = parent
+    #     self.children = children
+    #     self.time = time
 
 
 class TREE(Structure):
     _fields_ = [('num_leaves', c_long), ('tree', POINTER(NODE)),
                 ('root_time', c_long)]  # Everything from struct definition in C
 
-    def __init_(self, num_leaves, tree, root_time):
-        self.num_leaves = num_leaves
-        self.tree = tree
-        self.root_time = root_time
+    # def __init_(self, num_leaves, tree, root_time):
+    #     self.num_leaves = num_leaves
+    #     self.tree = tree
+    #     self.root_time = root_time
 
 
 class TREE_LIST(Structure):
     _fields_ = [('num_trees', c_int), ('trees', POINTER(TREE))]
 
-    def __init_(self, num_trees, trees):
-        self.num_trees = num_trees
-        self.trees = trees
+    # def __init_(self, num_trees, trees):
+    #     self.num_trees = num_trees
+    #     self.trees = trees
+
+
+def findpath_distance(t1, t2):
+
+    # This being called with two ete3 trees
+
+    # Then convert them or initialize the ctypes trees with them
+
+    lib.findpath_distance.argtypes = [POINTER(TREE), POINTER(TREE)]
+    return lib.findpath_distance(t1, t2)
 
 
 # C function tree_to_string (for testing purposes)
@@ -41,15 +49,14 @@ tree_to_cluster_string = lib.tree_to_string
 tree_to_cluster_string.argtypes = [POINTER(TREE)]
 tree_to_cluster_string.restype = c_char_p
 
-# C function findpath_distance
-findpath_distance = lib.findpath_distance
-findpath_distance.argtypes = [POINTER(TREE), POINTER(TREE)]
-findpath_distance.restype = c_long
 
 # C function return_findpath
 findpath_path = lib.return_findpath
 findpath_path.argtypes = [POINTER(TREE), POINTER(TREE)]
 findpath_path.restype = TREE_LIST
 
-pw_distances = lib.pw_distances
-pw_distances.argtypes = [POINTER(TREE_LIST), ndpointer(c_int, flags="C_CONTIGUOUS")]
+
+
+# ctypes.byref
+
+
