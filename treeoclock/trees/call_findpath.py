52e17a621bff8d72c3de05fb9af8e470684f3bab
Lars-B <berlinglars96@gmail.com>
2021-04-28 08:59:47 +0200

    findpath callable with ete3 trees

--- treeoclock/trees/call_findpath.py
+++ treeoclock/trees/call_findpath.py
@@ -34,14 +34,23 @@ class TREE_LIST(Structure):
     #     self.trees = trees
 
 
-def findpath_distance(t1, t2):
+def findpath_distance(t1, t2, c=False):
 
     # This being called with two ete3 trees
+    lib.findpath_distance.argtypes = [POINTER(TREE), POINTER(TREE)]
+
+    import tree_io
+
+    if c:
+        return lib.findpath_distance(t1, t2)
+
+    ct1 = tree_io.read_newick(t1.write(format=5))
+
+    ct2 = tree_io.read_newick(t2.write(format=5))
 
     # Then convert them or initialize the ctypes trees with them
 
-    lib.findpath_distance.argtypes = [POINTER(TREE), POINTER(TREE)]
-    return lib.findpath_distance(t1, t2)
+    return lib.findpath_distance(ct1, ct2)
 
 
 # C function tree_to_string (for testing purposes)
