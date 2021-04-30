caf903c4b289d229dba7568731121e61fa122521
Lars-B <berlinglars96@gmail.com>
2021-04-30 08:21:18 +0200

    Making the conversion more efficient

--- treeoclock/trees/call_findpath.py
+++ treeoclock/trees/call_findpath.py
@@ -1,7 +1,9 @@
 __author__ = 'Lena Collienne and Jordan Kettles'
 
 import os
+import sys
 from ctypes import *  # TODO
+import line_profiler
 
 lib = CDLL(f'{os.path.dirname(os.path.realpath(__file__))}/findpath.so')
 
@@ -26,59 +28,72 @@ class TREE(Structure):
         self.root_time = root_time
 
 
+# @profile
 def ete3_to_ctree(tree):
     num_leaves = len(tree.get_tree_root())
     num_nodes = (num_leaves * 2) - 1
     node_list = (NODE * num_nodes)()
 
-    distances = []
+    # leaves = [str(i+1) for i in range(num_leaves)]
 
-    tree_root = tree.get_tree_root()
+    distances = {}
 
+    # tree_root = tree.get_tree_root()
+
+    index = 0
     for node in tree.traverse('levelorder'):
-        if not node.is_leaf():
-            distances.append(node.get_distance(tree_root))
+        if node.children:
+            # if not node.is_leaf():
+            if index == 0:
+                node.name = node.dist
+            else:
+                # distances.append(node.get_distance(tree_root))  # get_distance() is a very slow function
+                node.name = node.dist + node.up.name  # Top down foor loop makes this possible
+            distances[node.name] = index
+            index += 1
 
-    distances = sorted(distances)
-    if not len(set(distances)) == num_leaves-1:
+    # distances = sorted(distances)
+    if not len(distances.keys()) == num_leaves - 1:
         sys.exit('Distances to root not unique! \n'
                  'This has to be resolved!')
     for node in tree.traverse('levelorder'):
-        if node.is_leaf():
-            node_list[int(node.name) - 1].parent = num_nodes - (distances.index(node.up.get_distance(tree_root)) + 1)
+        if not node.children:
+            # if node.is_leaf():
+            node_list[int(node.name) - 1].parent = num_nodes - (distances[node.up.name] + 1)
         else:
-            if node.is_root():
-                node_list[num_nodes - (distances.index(node.get_distance(tree_root)) + 1)].parent = num_nodes - 1
+            if node.name == 0.0:
+                node_list[num_nodes - (distances[node.name] + 1)].parent = num_nodes - 1
             else:
-                node_list[num_nodes - (distances.index(node.get_distance(tree_root)) + 1)].parent = \
-                    num_nodes - (distances.index(node.up.get_distance(tree_root)) + 1)
-            current_children = node.get_children()
+                node_list[num_nodes - (distances[node.name] + 1)].parent = \
+                    num_nodes - (distances[node.up.name] + 1)
+            # current_children = node.get_children()
+            current_children = node.children
             if len(current_children) != 2:
                 sys.exit('Not a binary tree!')
 
             # Child 0
-            if current_children[0].is_leaf():
-                node_list[num_nodes - (distances.index(node.get_distance(tree_root)) + 1)].children[0] = \
+            if not current_children[0].children:
+                # if current_children[0].is_leaf():
+                node_list[num_nodes - (distances[node.name] + 1)].children[0] = \
                     int(current_children[0].name) - 1
             else:
-                node_list[num_nodes - (distances.index(node.get_distance(tree_root)) + 1)].children[0] = \
-                    num_nodes - (distances.index(current_children[0].get_distance(tree_root)) + 1)
+                node_list[num_nodes - (distances[node.name] + 1)].children[0] = \
+                    num_nodes - (distances[current_children[0].name] + 1)
 
             # Child 1
-            if current_children[1].is_leaf():
-                node_list[num_nodes - (distances.index(node.get_distance(tree_root)) + 1)].children[1] = \
+            if not current_children[1].children:
+                # if current_children[1].is_leaf():
+                node_list[num_nodes - (distances[node.name] + 1)].children[1] = \
                     int(current_children[1].name) - 1
             else:
-                node_list[num_nodes - (distances.index(node.get_distance(tree_root)) + 1)].children[1] = \
-                    num_nodes - (distances.index(current_children[1].get_distance(tree_root)) + 1)
+                node_list[num_nodes - (distances[node.name] + 1)].children[1] = \
+                    num_nodes - (distances[current_children[1].name] + 1)
     return TREE(num_leaves, node_list, -1)
 
 
 def findpath_distance(t1, t2, c=False):
     # This being called with two ete3 trees
     lib.findpath_distance.argtypes = [POINTER(TREE), POINTER(TREE)]
-
-    import tree_io
 
     if c:
         return lib.findpath_distance(t1, t2)
@@ -102,11 +117,11 @@ class TREE_LIST(Structure):
     #     self.num_trees = num_trees
     #     self.trees = trees
 
+
 # C function tree_to_string (for testing purposes)
 tree_to_cluster_string = lib.tree_to_string
 tree_to_cluster_string.argtypes = [POINTER(TREE)]
 tree_to_cluster_string.restype = c_char_p
-
 
 # C function return_findpath
 findpath_path = lib.return_findpath
