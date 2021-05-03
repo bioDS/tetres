03da8d6a41502e0dc96caeb131797e14d846a271
Lars-B <berlinglars96@gmail.com>
2021-05-03 15:46:54 +0200

    Some work on documenation, outline for the trees subpackage

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -55,3 +56,4 @@ html_theme = 'bizstyle'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
