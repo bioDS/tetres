f72f500f239c1bdc8983b9b9e34ecaefd811df73
Lars-B <berlinglars96@gmail.com>
2021-05-07 13:51:12 +0200

    Testing for findpath_distance

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,10 @@ def five_taxa_newick_list():
     # list of pairwise distances is [0, 1, 2, 1, 0, 3, 2, 3, 0]
     return ["((1:3,5:3):1,(4:2,(3:1,2:1):1):2);", "((1:2,5:2):3,(4:3,(3:1,2:1):2):2);",
             "((1:2,5:2):3,(3:3,(4:1,2:1):2):2);"]
+
+@pytest.fixture
+def five_taxa_newick_list_distances():
+    return [0, 1, 2, 1, 0, 3, 2, 3, 0]
+
+
+
---
+++ tests/tests_trees/test_time_trees.py
@@ -0,0 +1,49 @@
+import ete3
+
+
+from treeoclock.trees.time_trees import TimeTree, findpath_distance
+from treeoclock.trees._converter import ete3_to_ctree
+
+def test_fp_distance(five_taxa_newick_list, five_taxa_newick_list_distances):
+    out = []
+    t = [TimeTree(i) for i in five_taxa_newick_list]
+    for i in t:
+        out.extend([i.fp_distance(j) for j in t])
+    assert out == five_taxa_newick_list_distances, f'fp_distance wrong {out}'
+
+
+def test_fp_path():
+    # TODO
+    assert True
+
+
+def test_get_newick():
+    # TODO
+    assert True
+
+
+def test_findpath_distance_timetree(five_taxa_newick_list, five_taxa_newick_list_distances):
+    t = [TimeTree(i) for i in five_taxa_newick_list]
+    out = []
+    for i in t:
+        for j in t:
+            out.append(findpath_distance(i, j))
+    assert out == five_taxa_newick_list_distances, f"findpath_distances for TimeTree class wrong {out}"
+
+
+def test_findpath_distance_ctree(five_taxa_newick_list, five_taxa_newick_list_distances):
+    t = [ete3.Tree(i) for i in five_taxa_newick_list]
+    out = []
+    for i in t:
+        for j in t:
+            out.append(findpath_distance(i, j))
+    assert out == five_taxa_newick_list_distances, f"findpath_distances for ete3.Tree class wrong {out}"
+
+
+def test_findpath_distance_ete3tree(five_taxa_newick_list, five_taxa_newick_list_distances):
+    t = [ete3_to_ctree(ete3.Tree(i)) for i in five_taxa_newick_list]
+    out = []
+    for i in t:
+        for j in t:
+            out.append(findpath_distance(i, j))
+    assert out == five_taxa_newick_list_distances, f"findpath_distances for c-TREE class wrong {out}"
