3e216ef000343e402e5030e50ef9506531ac7b95
Lars-B <berlinglars96@gmail.com>
2021-05-25 12:16:43 +0200

    Added some more documentation and test hints

--- tests/test_centroid/test_compute_sos.py
+++ tests/test_centroid/test_compute_sos.py
@@ -6,16 +6,16 @@ from treeoclock.trees.time_trees import TimeTree, TimeTreeSet
 def test_compute_sos_one_core(dir, five_taxa_nexus_string):
     dir.write("test.nex", five_taxa_nexus_string)
     trees = TimeTreeSet(f'{dir.path}/test.nex')
-    assert compute_sos(t=trees[0], trees=trees, n_cores=1) == 5, "Compute_sos failed, should be 0!"
+    assert compute_sos(t=trees[0], trees=trees, n_cores=1) == 5, "Compute_sos failed (1=cores), should be 0!"
 
 
 def test_compute_sos_none_core(dir, five_taxa_nexus_string):
     dir.write("test.nex", five_taxa_nexus_string)
     trees = TimeTreeSet(f'{dir.path}/test.nex')
-    assert compute_sos(t=trees[0], trees=trees, n_cores=None) == 5, "Compute_sos failed, should be 0!"
+    assert compute_sos(t=trees[0], trees=trees, n_cores=None) == 5, "Compute_sos failed (None=cores), should be 0!"
 
 
 def test_compute_sos_four_cores(dir, five_taxa_nexus_string):
     dir.write("test.nex", five_taxa_nexus_string)
     trees = TimeTreeSet(f'{dir.path}/test.nex')
-    assert compute_sos(t=trees[0], trees=trees, n_cores=4) == 5, "Compute_sos failed, should be 0!"
+    assert compute_sos(t=trees[0], trees=trees, n_cores=4) == 5, "Compute_sos failed (4=cores), should be 0!"
--- treeoclock/summary/compute_sos.py
+++ treeoclock/summary/compute_sos.py
@@ -3,7 +3,19 @@ from multiprocessing import Pool
 from ..trees.time_trees import TimeTree, TimeTreeSet, findpath_distance
 
 
-def compute_sos(t: TimeTree, trees: TimeTreeSet, n_cores: int):
+def compute_sos(t: TimeTree, trees: TimeTreeSet, n_cores: int) -> int:
+    """
+    Computes the sum of squared distances for the tree t and the set trees, using n_cores processing cores
+
+    :param t: A tree
+    :type t: TimeTree
+    :param trees: A set of trees
+    :type trees: TimeTreeSet
+    :param n_cores: Number of processing cores
+    :type n_cores: int
+    :return: The sum of squared distances
+    :rtype: int
+    """
     with Pool(n_cores) as p:
         dists = p.starmap(findpath_distance, [(t.etree, i.etree) for i in trees])
-    return sum(i*i for i in dists)
+    return sum(i * i for i in dists)
