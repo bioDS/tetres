8863631b7e2753d46013d7a1ac1f9d5e50ee21ce
Lars-B <berlinglars96@gmail.com>
2021-06-01 09:36:34 +0200

    Reverting some changes

--- tests/test_summary/test_centroid.py
+++ tests/test_summary/test_centroid.py
@@ -9,26 +9,23 @@ def test_centroid_construction_empty():
 
 
 def test_centroid_construction():
-    assert Centroid(variation="inc-sub", n_cores=None, start='first'), "Construction not possible!"
+    assert Centroid(variation="inc-sub"), "Construction not possible!"
 
 
 def test_centroid_compute_centroid_var_error():
     with pytest.raises(ValueError):
-        Centroid(variation="TEST", n_cores=1)
+        Centroid(variation="TEST", n_cores=1).compute_centroid(TimeTreeSet())
 
 
 def test_centroid_compute_centroid_n_cores_error():
     with pytest.raises(ValueError):
-        Centroid(variation="greedy", n_cores=0.6)
+        Centroid(variation="greedy", n_cores=0.9).compute_centroid(TimeTreeSet())
 
 
 def test_centroid_greedy():
     # TODO Needs to be adapted
-    assert Centroid(variation="greedy", n_cores=4).compute_centroid(TimeTreeSet()) == 0, "Greedy Variation Failed!"
+    assert Centroid(variation="greedy").compute_centroid(TimeTreeSet()) == 0, "Greedy Variation Failed"
 
 
-def test_centroid_n_cores_none():
-    # TODO Needs to be adapted
-    assert Centroid(variation="greedy").compute_centroid(TimeTreeSet()) == 0, "Greedy with n_cores=None failed!"
 
 
--- treeoclock/summary/centroid.py
+++ treeoclock/summary/centroid.py
@@ -1,7 +1,5 @@
-
-
 from . import _variations
-from ._constants import VAR_LIST
+from ._constants import VAR_LIST, START_LIST
 from treeoclock.trees.time_trees import TimeTreeSet
 
 
@@ -13,23 +11,25 @@ class Centroid:
     Class to compute the centroid algorithm with a set of parameters
     """
 
-    def __init__(self, variation="inc-sub", n_cores=None):
-        self.variation = variation
-        self.n_cores = n_cores
+    def __init__(self, variation="inc-sub", n_cores=None, start='random'):
+        self.variation = variation  # Which centroid variation to compute
+        self.n_cores = n_cores  # How many cores to use whenever Multiprocessing is/will be used
+        self.start = start  # Specifying which tree to choose in case of multiple options with the same quality
+        # depending on the variation if it will be used
 
     def compute_centroid(self, trees: TimeTreeSet):
 
-        # TODO this error may move to the actual centroid function
+        # Checking if all parameters are correctly set up
         if self.variation not in VAR_LIST:
             raise ValueError(f"The 'variation' parameter should be"
                              f" in {VAR_LIST} but {self.variation} was given.")
-        if not isinstance(self.n_cores, (int)):
+        if not (isinstance(self.n_cores, int) or self.n_cores is None):
             raise ValueError(f"The 'n_cores' parameter should be an integer"
                              f" but {self.n_cores} was given.")
+        if self.start not in START_LIST:
+            raise ValueError(f"The 'variation' parameter should be"
+                             f" in {START_LIST} but {self.start} was given.")
 
-        # TODO the starting  tree needs a parameter list [fm, efm, last, random,]
-
-        # TODO Argument trees need to be passed
         return getattr(_variations, self.variation)(trees)
 
         return 0
