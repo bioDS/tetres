5a64cf602fd754c86f5c6a14687ebcffd7bee17a
Lars-B <berlinglars96@gmail.com>
2021-06-01 13:02:19 +0200

    Adding raised exception if no tree is found in the neighbourhood

--- treeoclock/summary/_tree_proposals.py
+++ treeoclock/summary/_tree_proposals.py
@@ -5,6 +5,11 @@ from treeoclock.summary._constants import SELECT_LIST
 from random import choice
 
 
+class NoBetterNeighbourFound(Exception):
+    """Raised when no neighbour has a better SOS value"""
+    pass
+
+
 def search_neighbourhood_greedy(t: TimeTree, trees: TimeTreeSet, t_value: int, n_cores=None, select='first'):
     neighbourhood = t.neighbours()
     better_neighbours = []
@@ -17,17 +22,20 @@ def search_neighbourhood_greedy(t: TimeTree, trees: TimeTreeSet, t_value: int, n
         elif sos == cur_best and sos < t_value:
             better_neighbours.append(n)
 
-    if len(better_neighbours) == 1:
+    if len(better_neighbours) == 0:
+        # No better neighbour was found
+        raise NoBetterNeighbourFound
+    elif len(better_neighbours) == 1:
         # Only one neighbour with smallest value found
-        return better_neighbours[0]
+        return better_neighbours[0], cur_best
 
     # multiple neighbours with same SOS value found, choosing by the given selection method
     if select == 'first':
-        return better_neighbours[0]
+        return better_neighbours[0], cur_best
     elif select == 'last':
-        return better_neighbours[-1]
+        return better_neighbours[-1], cur_best
     elif select == 'random':
-        return choice(better_neighbours)
+        return choice(better_neighbours), cur_best
 
     raise ValueError(f"The 'select' parameter should be"
                      f" in {SELECT_LIST} but {select} was given.")
