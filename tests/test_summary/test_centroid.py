6186bc8e0f475511dafa21e56860d7f073ead545
Lars-B <berlinglars96@gmail.com>
2021-06-02 09:12:15 +0200

    Added test for start parameter and changed construction test

--- tests/test_summary/test_centroid.py
+++ tests/test_summary/test_centroid.py
@@ -9,7 +9,7 @@ def test_centroid_construction_empty():
 
 
 def test_centroid_construction():
-    assert Centroid(variation="inc-sub"), "Construction not possible!"
+    assert Centroid(variation="greedy", n_cores=4, select='last', start='first'), "Construction not possible!"
 
 
 def test_centroid_compute_centroid_var_error():
@@ -22,10 +22,21 @@ def test_centroid_compute_centroid_n_cores_error():
         Centroid(variation="greedy", n_cores=0.9).compute_centroid(TimeTreeSet())
 
 
-def test_centroid_greedy():
-    # TODO Needs to be adapted
-    assert Centroid(variation="greedy").compute_centroid(TimeTreeSet()) == 0, "Greedy Variation Failed"
+def test_centroid_compute_centroid_select_error():
+    with pytest.raises(ValueError):
+        Centroid(variation="TEST", n_cores=1, select='test').compute_centroid(TimeTreeSet())
 
 
+def test_centroid_compute_centroid_start_error():
+    with pytest.raises(ValueError):
+        Centroid(variation="TEST", n_cores=1, start='test').compute_centroid(TimeTreeSet())
 
 
+def test_centroid_compute_centroid_start_oor_error():
+    with pytest.raises(ValueError):
+        Centroid(variation="TEST", n_cores=1, start=1).compute_centroid(TimeTreeSet())
+
+
+# def test_centroid_greedy():
+#     # TODO Needs to be adapted
+#     assert Centroid(variation="greedy").compute_centroid(TimeTreeSet()) == 0, "Greedy Variation Failed"
