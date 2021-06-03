c4bebdbed56135a6cf917448b77329399de16a56
Lars-B <berlinglars96@gmail.com>
2021-06-03 08:45:58 +0200

    Minor Changes, fixing bug

--- treeoclock/summary/frechet_mean.py
+++ treeoclock/summary/frechet_mean.py
@@ -18,9 +18,10 @@ def frechet_mean(trees: Union[TimeTreeSet, list]):
         cur_tree = working_tree_set.pop()
         # cur_path = frechet_mean.fp_path(cur_tree)
         cur_path = findpath_path(frechet_mean, cur_tree.ctree)
+        # Only consider to break if the tree isn't close to the current FM tree
         if int(len(cur_path) / fm_divider) == 0:
             break
-        # frechet_mean = ctree_to_ete3(cur_path[int(len(cur_path)/fm_divider)])
+        frechet_mean = cur_path[int(len(cur_path)/fm_divider)]
         # Assigning the new FM as the tree 1/fm_divider of the distances to the cur_tree
         fm_divider += 1
 
@@ -28,14 +29,14 @@ def frechet_mean(trees: Union[TimeTreeSet, list]):
 
 
 if __name__ == '__main__':
-    d_name = "Dengue"
+    d_name = "RSV2"
 
     myts = TimeTreeSet(f'/Users/larsberling/Desktop/CodingMA/Git/Summary/MDS_Plots/{d_name}/{d_name}.trees')
 
     for _ in range(5):
         fm = frechet_mean(myts)
-        print(fm.get_newick())
+        # print(fm.get_newick())
 
     for _ in range(5):
         fm = frechet_mean(myts[1:2])
-        print(fm.get_newick())
+        # print(fm.get_newick())
