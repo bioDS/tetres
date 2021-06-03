1d5caabb0cb9f2f78663974b2fc36b7331c52b9d
Lars-B <berlinglars96@gmail.com>
2021-06-03 08:51:11 +0200

    Changed to use the TimeTreeSet test fixture instead

--- tests/test_summary/test_tree_proposals.py
+++ tests/test_summary/test_tree_proposals.py
@@ -3,22 +3,19 @@ from treeoclock.summary._tree_proposals import search_neighbourhood_greedy
 from treeoclock.trees.time_trees import TimeTreeSet
 
 
-def test_search_neighbourhood_greedy_ncorenone(dir, five_taxa_nexus_string):
-    dir.write("test.nex", five_taxa_nexus_string)
-    trees = TimeTreeSet(f'{dir.path}/test.nex')
-    res = search_neighbourhood_greedy(t=trees[1], trees=trees, t_value=10)
-    assert trees[0].fp_distance(res) == 0, "Greedy neighbourhood search with n_cores=None failed!"
+def test_search_neighbourhood_greedy_ncorenone(five_taxa_tts):
+    res = search_neighbourhood_greedy(t=five_taxa_tts[1], trees=five_taxa_tts, t_value=10)
+    assert (five_taxa_tts[0].fp_distance(res[0]), res[1]) == (0, 5), "Greedy neighbourhood search with n_cores=None failed!"
 
 
-def test_search_neighbourhood_greedy_ncore1(dir, five_taxa_nexus_string):
-    dir.write("test.nex", five_taxa_nexus_string)
-    trees = TimeTreeSet(f'{dir.path}/test.nex')
-    res = search_neighbourhood_greedy(t=trees[1], trees=trees, t_value=10, n_cores=1)
-    assert trees[0].fp_distance(res) == 0, "Greedy neighbourhood search with n_cores=1 failed!"
+def test_search_neighbourhood_greedy_ncore1(five_taxa_tts):
+    res = search_neighbourhood_greedy(t=five_taxa_tts[1], trees=five_taxa_tts, t_value=10, n_cores=1)
+    assert (five_taxa_tts[0].fp_distance(res[0]), res[1]) == (0, 5), "Greedy neighbourhood search with n_cores=1 failed!"
 
 
-def test_search_neighbourhood_greedy_ncore4(dir, five_taxa_nexus_string):
-    dir.write("test.nex", five_taxa_nexus_string)
-    trees = TimeTreeSet(f'{dir.path}/test.nex')
-    res = search_neighbourhood_greedy(t=trees[1], trees=trees, t_value=10, n_cores=4)
-    assert trees[0].fp_distance(res) == 0, "Greedy neighbourhood search with n_cores=4 failed!"
+def test_search_neighbourhood_greedy_ncore4(five_taxa_tts):
+    res = search_neighbourhood_greedy(t=five_taxa_tts[1], trees=five_taxa_tts, t_value=10, n_cores=4)
+    assert (five_taxa_tts[0].fp_distance(res[0]), res[1]) == (0, 5), "Greedy neighbourhood search with n_cores=4 failed!"
+
+# TODO test for the selection methods, first construct a test case with multiple choices and then test those
+#  also test for the value error that gets raised if wrong select is given
