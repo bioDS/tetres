279cceecebc234f2c56ac66f01872aefba8a84f0
Lars-B <berlinglars96@gmail.com>
2021-06-22 06:47:13 +0200

    Returns a centroid TimeTree and an SoS value

--- treeoclock/summary/_variations.py
+++ treeoclock/summary/_variations.py
@@ -4,17 +4,23 @@ from treeoclock.summary._tree_proposals import search_neighbourhood_greedy, NoBe
 
 
 def greedy(trees: TimeTreeSet, n_cores: int, select: str, start: TimeTree):
-    # parameter for the starting tree, or just a starting tree given
 
-    initial_sos = compute_sos_mt(start, trees, n_cores=n_cores)
+    sos = compute_sos_mt(start, trees, n_cores=n_cores)
     centroid = start
 
     while True:
         try:
-            centroid, initial_sos = search_neighbourhood_greedy(t=centroid, trees=trees, t_value=initial_sos,
+            centroid, sos = search_neighbourhood_greedy(t=centroid, trees=trees, t_value=sos,
                                                                 n_cores=n_cores, select=select)
         except NoBetterNeighbourFound:
             # This is thrown when no neighbour has a better SoS value, i.e. the loop can be stopped
             break
+    return centroid, sos
 
-    return centroid
+
+def inc_sub(trees: TimeTreeSet, n_cores: int, select: str, start: TimeTree):
+
+    # TODO how to get the subsample size ? via ESS computation ?
+    initial_sos = compute_sos_mt(start, trees, n_cores=n_cores)
+    # TODO WIP still need the common subtrees for specific selection of neighbours!!!
+
