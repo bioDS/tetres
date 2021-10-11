38e6f822c79bb34ff2a4ef74b7258e780e4a9bf5
Lars-B <berlinglars96@gmail.com>
2021-10-11 12:26:54 +0200

    Added frechet mean test with fixed random seed

--- tests/test_summary/test_frechet_mean.py
+++ tests/test_summary/test_frechet_mean.py
@@ -1,9 +1,20 @@
-from treeoclock.trees.time_trees import TimeTreeSet, TimeTree, findpath_distance
+from treeoclock.trees.time_trees import TimeTree, findpath_distance
 from treeoclock.summary.frechet_mean import frechet_mean
+from treeoclock.summary.compute_sos import compute_sos
 
+import random
 
 def test_frechet_mean(five_taxa_tts):
     fm = frechet_mean([five_taxa_tts[0], five_taxa_tts[2]])
     tfm = TimeTree('((4:2,(3:1,5:1):1):2,(1:3,2:3):1);')
     d = [findpath_distance(tfm, i) for i in [five_taxa_tts[0], five_taxa_tts[2], fm]]
     assert d == [1, 1, 0], 'Frechet Mean computation failed!'
+
+
+def test_frechet_mean_data(twelve_taxa_tts):
+    state = random.getstate()  # get the random seed state
+    random.seed(10)  # Fixing the seed to get the same result
+    fm = frechet_mean(twelve_taxa_tts)
+    sos = compute_sos(fm, twelve_taxa_tts)
+    random.setstate(state)  # reset the random seed to previous state
+    assert sos == 351747, "Frechet Mean failed on data set!"
