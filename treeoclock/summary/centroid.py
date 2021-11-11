9e04e458e1ba9d89dff7e6ac822517e240e84539
Lars-B <berlinglars96@gmail.com>
2021-11-11 09:28:22 +0100

    Added TTS starting option

--- treeoclock/summary/centroid.py
+++ treeoclock/summary/centroid.py
@@ -13,7 +13,6 @@ class Centroid:
     Class to compute the centroid algorithm with a set of parameters
     """
 
-    # TODO Default start is going to be FM, not implemented yet
     def __init__(self, variation="greedy", n_cores=None, select='random', start='FM', subsample_size=200):
         self.variation = variation  # Which centroid variation to compute
         self.n_cores = n_cores  # How many cores to use whenever Multiprocessing is/will be used
@@ -33,9 +32,13 @@ class Centroid:
         if self.select not in SELECT_LIST:
             raise ValueError(f"The 'select' parameter should be"
                              f" in {SELECT_LIST} but {self.select} was given.")
-        if not (self.start in START_LIST or (isinstance(self.start, int) and self.start in range(len(trees)))):
+        # TODO self.start isinstance(TimeTree) and then this tree will be the starting tree
+        #  Check for the mapping, and if not correct change accordingly
+        if not (self.start in START_LIST or (isinstance(self.start, int) and self.start in range(len(trees)))
+                or isinstance(self.start, TimeTreeSet)):
             raise ValueError(f"The 'start' parameter should be in {START_LIST} or "
-                             f"an integer between 0 and {len(trees)-1}!")
+                             f"an integer between 0 and {len(trees)-1} or"
+                             f"a TimeTreeSet containing 1 tree!")
 
         # Computing/selecting the starting tree with the specified variation
         starting_tree = trees[0]  # Initializing with the case "first"
@@ -48,30 +51,45 @@ class Centroid:
             starting_tree = trees[self.start]
         elif self.start == "FM":
             starting_tree = frechet_mean(trees)
+        elif isinstance(self.start, TimeTreeSet):
+            if not len(self.start) == 1:
+                raise ValueError("Starting with a TimeTreeSet only possible if it contains a single tree!")
+                # maybe change this in the future
+            if not len(trees[0]) == len(self.start[0]):
+                raise ValueError(f"The given starting tree has different number of taxa! "
+                                 f"({len(trees[0])} vs. {len(self.start[0])})")
+            # todo check the mapping and change accordingly if nessecary
+            starting_tree = self.start[0]  # Setting the start to the only tree in the set
 
         return getattr(_variations, self.variation)(trees=trees, n_cores=self.n_cores, select=self.select,
                                                     start=starting_tree, subsample_size=self.subsample_size)
 
 
 if __name__ == '__main__':
-    d_name = "Dengue"
-
-    myts = TimeTreeSet(f'/Users/larsberling/Desktop/CodingMA/Git/Summary/MDS_Plots/{d_name}/{d_name}.trees')
-
-    mycen = Centroid(start="FM", variation="inc_sub")
 
     from timeit import default_timer as timer
 
     import numpy as np
 
-    times = []
-    for _ in range(10):
-        s = timer()
-        cen, sos = mycen.compute_centroid(myts)
-        print(sos)
-        times.append(timer()-s)
-    print(np.mean(times))
+    # d_name = "binary_K047_no_error_including_burnin"
 
-    # 22485 all
-    # 1.2970010868 seconds for 10
+    mystart = TimeTreeSet('/Users/larsberling/Desktop/CodingMA/Git/Summary/Simulations/31_800_005_0/conv_centroid.tree')
 
+    myts = TimeTreeSet('/Users/larsberling/Desktop/CodingMA/Git/Summary/Simulations/31_800_005_0/31_800_005_0.trees')
+
+    mycen = Centroid(start=mystart, variation="inc_sub")
+
+    cen, sos = mycen.compute_centroid(myts)
+
+    print(sos)
+
+    # d_name = 'RSV2'
+    #
+    # for i in range(2):
+    #     myts = TimeTreeSet(f'/Users/larsberling/Desktop/CodingMA/Git/Summary/MDS_Plots/{d_name}/{d_name}.trees')
+    #
+    #     mycen = Centroid(start="FM", variation="inc_sub")
+    #
+    #     cen, sos = mycen.compute_centroid(myts)
+    #
+    #     print(sos)
