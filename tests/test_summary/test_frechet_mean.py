954836c3e4138c3c48aeddd10c59e5f1bb5430b0
Lars-B <berlinglars96@gmail.com>
2022-04-20 10:34:08 +1200

    Adapting tests to use mt sos version, update sos value

--- tests/test_summary/test_frechet_mean.py
+++ tests/test_summary/test_frechet_mean.py
@@ -1,6 +1,6 @@
 from treeoclock.trees.time_trees import TimeTree, findpath_distance
 from treeoclock.summary.frechet_mean import frechet_mean, frechet_mean_sort
-from treeoclock.summary.compute_sos import compute_sos
+from treeoclock.summary.compute_sos import compute_sos_mt
 
 import random
 
@@ -15,12 +15,12 @@ def test_frechet_mean_data(twelve_taxa_tts):
     state = random.getstate()  # get the random seed state
     random.seed(10)  # Fixing the seed to get the same result
     fm = frechet_mean(twelve_taxa_tts)
-    sos = compute_sos(fm, twelve_taxa_tts)
+    sos = compute_sos_mt(fm, twelve_taxa_tts)
     random.setstate(state)  # reset the random seed to previous state
-    assert sos == 351747, "Frechet Mean failed on data set!"
+    assert sos == 448947, "Frechet Mean failed on data set!"
 
 
 def test_frechet_mean_sort(twelve_taxa_tts):
     fm = frechet_mean_sort(twelve_taxa_tts)
-    sos = compute_sos(fm, twelve_taxa_tts)
+    sos = compute_sos_mt(fm, twelve_taxa_tts)
     assert sos == 289661, "Frechet mean sort failed!"
