a6747ad6c995acf46fdcc3cb91253430f2e24bbb
Lars-B <berlinglars96@gmail.com>
2022-05-03 11:04:58 +1200

    Bug fixing MAD functions

--- treeoclock/enums.py
+++ treeoclock/enums.py
@@ -2,9 +2,9 @@ from enum import Enum
 import numpy as np
 
 
-def _median_ad(self, *args):
+def _median_ad(*args):
     return np.median(np.absolute(args - np.median(args)))
-def _mean_ad(self, *args):
+def _mean_ad(*args):
     return np.mean(np.absolute(args - np.mean(args)))
 
 
@@ -15,8 +15,6 @@ class Summary(Enum):
 
 
 class Avg_enum(Enum):
-    # def __call__(self, *args):
-    #     return self.value(*args)
     MEAN = np.mean
     MEDIAN = np.median
     MEDIAN_AD = _median_ad
