d414bc3d058c36d1b1654fff28566b3a21ba3f68
Lars-B <berlinglars96@gmail.com>
2022-05-16 10:42:01 +1200

    Fixing warning and only computing upper triagonal matrix

--- treeoclock/trees/findpath.c
+++ treeoclock/trees/findpath.c
@@ -437,17 +437,16 @@ Pair best_sos_neighbour(Tree_List *input_trees, Tree_List *tree_set, int n_cores
     return ret_pair;
 }
 
-
-void pairwise_distance(Tree_List *input_trees, long n, long *dm[n][n])
+// long (*dm)[n][n]
+void pairwise_distance(Tree_List *input_trees, long n, long dm[][n])
 {
     #pragma omp parallel
         for(int i=0; i < n-1; i++)
         {
             for (int j=i; j < n; j++)
             {
-                long cur_d = findpath_distance(&input_trees->trees[i], &input_trees->trees[j]);
-                dm[i][j] = cur_d;
-                dm[j][i] = cur_d;
+//                long cur_d = findpath_distance(&input_trees->trees[i], &input_trees->trees[j]);
+                dm[i][j] = findpath_distance(&input_trees->trees[i], &input_trees->trees[j]);
             }
         }
 }
