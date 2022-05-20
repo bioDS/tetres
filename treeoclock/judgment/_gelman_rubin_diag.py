01e183b1f1dfed5547c0bf01e32c762db705e8e0
Lars-B <berlinglars96@gmail.com>
2022-05-20 13:20:38 +1200

    Adding option samples="all"

--- treeoclock/judgment/_gelman_rubin_diag.py
+++ treeoclock/judgment/_gelman_rubin_diag.py
@@ -1,25 +1,29 @@
-from treeoclock.trees.time_trees import TimeTreeSet, findpath_distance
+from treeoclock.trees.time_trees import TimeTreeSet
 from treeoclock.judgment._pairwise_distance_matrix import calc_pw_distances, calc_pw_distances_two_sets
 
 import random
 import numpy as np
 import seaborn as sns
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
 def gelman_rubin_distance_diagnostic_plot(cMChain, samples: int = 100):
 
     figure, axis = plt.subplots(nrows=cMChain.m_MChains, ncols=cMChain.m_MChains, constrained_layout=True, figsize=[9, 7])
+
+    # todo a cMChain initialize pwd_matrices using multiprocessing?
+
     for i in range(cMChain.m_MChains-1):
         for j in range(i+1, cMChain.m_MChains):
             cur_psrf_like = gelman_rubin_distance_diagnostic_from_matrices(cMChain.pwd_matrix(i),
                                                                            cMChain.pwd_matrix(j),
                                                                            cMChain.pwd_matrix(i, j),
                                                                            samples=samples)
-            sns.kdeplot(ax=axis[i, j], data=cur_psrf_like, fill=True)
+            sns.kdeplot(ax=axis[i, j], data=cur_psrf_like, x="PSRF_like", hue="Treeset", fill=True, legend=False)
             for label in axis[i, j].get_xticklabels():
                 label.set_rotation(45)
-            sns.boxplot(ax=axis[j, i], data=cur_psrf_like)
+            sns.boxplot(ax=axis[j, i], data=cur_psrf_like, y="PSRF_like", hue="Treeset")
             # todo axis[i, i] some plot on diagonal missing
 
     # Annotation of the plot
@@ -36,7 +40,7 @@ def gelman_rubin_distance_diagnostic_plot(cMChain, samples: int = 100):
                     xycoords=ax.yaxis.label, textcoords="offset points",
                     size="large", ha="right", va="center")
 
-    plt.savefig(fname=f"{cMChain.working_dir}/{cMChain.name}_grd_plot.png", format="png", bbox_inches="tight", dpi=800)
+    plt.savefig(fname=f"{cMChain.working_dir}/{cMChain.name}_grd_plot{'' if samples == 100 else f'_{samples}'}.png", format="png", bbox_inches="tight", dpi=800)
 
 
 def gelman_rubin_distance_diagnostic_from_matrices(pwts1, pwts2, pwts1ts2,
@@ -45,18 +49,29 @@ def gelman_rubin_distance_diagnostic_from_matrices(pwts1, pwts2, pwts1ts2,
     nt2 = pwts2.shape[0]
     psrf_like = []
 
-    for _ in range(samples):
-        ts1_sample = random.randint(0, nt1 - 1)
-        in_sample_var = np.sum(pwts1[ts1_sample, :]) + np.sum(pwts1[:, ts1_sample])
-        between_sample_var = np.sum(pwts1ts2[ts1_sample, :])
-        psrf_like.append(np.sqrt((between_sample_var / nt2) / (in_sample_var / nt1)))
+    if samples != "all":
+        for _ in range(samples):
+            ts1_sample = random.randint(0, nt1 - 1)
+            in_sample_var = np.sum(pwts1[ts1_sample, :]) + np.sum(pwts1[:, ts1_sample])
+            between_sample_var = np.sum(pwts1ts2[ts1_sample, :])
+            psrf_like.append(["TS1", np.sqrt((between_sample_var / nt2) / (in_sample_var / nt1))])
 
-        ts2_sample = random.randint(0, nt2 - 1)
-        in_sample_var = np.sum(pwts2[ts2_sample, :]) + np.sum(pwts2[:, ts2_sample])
-        between_sample_var = np.sum(pwts1ts2[:, ts2_sample])
-        psrf_like.append(np.sqrt((between_sample_var / nt1) / (in_sample_var / nt2)))
-
-    return psrf_like
+            ts2_sample = random.randint(0, nt2 - 1)
+            in_sample_var = np.sum(pwts2[ts2_sample, :]) + np.sum(pwts2[:, ts2_sample])
+            between_sample_var = np.sum(pwts1ts2[:, ts2_sample])
+            psrf_like.append(["TS2", np.sqrt((between_sample_var / nt1) / (in_sample_var / nt2))])
+    elif samples == "all":
+        for cur_sample in range(nt1):
+            in_sample_var = np.sum(pwts1[cur_sample, :]) + np.sum(pwts1[:, cur_sample])
+            between_sample_var = np.sum(pwts1ts2[cur_sample, :])
+            psrf_like.append(["TS1", np.sqrt((between_sample_var / nt2) / (in_sample_var / nt1))])
+        for cur_sample in range(nt2):
+            in_sample_var = np.sum(pwts2[cur_sample, :]) + np.sum(pwts2[:, cur_sample])
+            between_sample_var = np.sum(pwts1ts2[:, cur_sample])
+            psrf_like.append(["TS2", np.sqrt((between_sample_var / nt1) / (in_sample_var / nt2))])
+    else:
+        raise ValueError(f"Unrecognizes samples {samples} given!")
+    return pd.DataFrame(data=psrf_like, columns=["Treeset", "PSRF_like"])
 
 
 def gelman_rubin_distance_diagnostic(tree_set1: TimeTreeSet, tree_set2: TimeTreeSet,
