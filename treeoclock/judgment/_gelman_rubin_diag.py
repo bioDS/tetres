a38e737098d3262d85f2106c1896848eda9b966d
Lars-B <berlinglars96@gmail.com>
2022-05-26 10:43:59 +1200

    fixing bug for geweke part on diagonal

--- treeoclock/judgment/_gelman_rubin_diag.py
+++ treeoclock/judgment/_gelman_rubin_diag.py
@@ -56,11 +56,11 @@ def gelman_rubin_distance_diagnostic_plot(cMChain, samples: int = 100):
             # print(ks)
 
         cur_geweke = cMChain.MChain_list[i].compute_geweke_distances(index=i, name=cMChain.name, add=False)
-        cur_sample = range(0, cMChain.MChain_list[i].chain_length, cMChain.MChain_list[i].tree_sampling_interval)
+        cur_sample = range(0, cMChain[i].chain_length + cMChain[i].tree_sampling_interval, cMChain[i].tree_sampling_interval)
         sns.lineplot(ax=axis[i, i], y=cur_geweke, x=cur_sample)
     # adding the last diagonal plot, because i will not run far enough
-    cur_geweke = cMChain.MChain_list[cMChain.m_MChains-1].compute_geweke_distances(index=cMChain.m_MChains-1, name=cMChain.name, add=False)
-    cur_sample = range(0, cMChain.MChain_list[i].chain_length, cMChain.MChain_list[i].tree_sampling_interval)
+    cur_geweke = cMChain[-1].compute_geweke_distances(index=cMChain.m_MChains-1, name=cMChain.name, add=False)
+    cur_sample = range(0, cMChain[-1].chain_length + cMChain[-1].tree_sampling_interval, cMChain[-1].tree_sampling_interval)
     sns.lineplot(ax=axis[cMChain.m_MChains-1, cMChain.m_MChains-1], y=cur_geweke, x=cur_sample)
 
         
@@ -114,35 +114,14 @@ def gelman_rubin_distance_diagnostic_from_matrices(pwts1, pwts2, pwts1ts2,
     return pd.DataFrame(data=psrf_like, columns=["Treeset", "PSRF_like"])
 
 
-def gelman_rubin_distance_diagnostic(tree_set1: TimeTreeSet, tree_set2: TimeTreeSet,
-                                     samples: int = 100):
-    # todo time the distance matrix computation and text remco how long it takes to do this
-    distances_pwts1 = calc_pw_distances(tree_set1)
-    distances_pwts2 = calc_pw_distances(tree_set2)
-    distance_pwts1ts2 = calc_pw_distances_two_sets(tree_set1, tree_set2)
+def gelman_rubin_diagnostic_list(cMChain, i, j):
+    # Will compute the gelman rubin like diagnostic for chains i and j, this will result in a 'trace'
 
-    # var_difference = []
-    # in_sample_var_list = []
-    # between_sample_var_list = []
-    psrf_like = []
+    # todo check index i and j for validity
 
-    for _ in range(samples):
-        ts1_sample = random.randint(0, len(tree_set1) - 1)
+    # todo check that both chains have same sized tree set
 
-        in_sample_var = np.sum(distances_pwts1[ts1_sample, :]) + np.sum(distances_pwts1[:, ts1_sample])
 
-        between_sample_var = np.sum(distance_pwts1ts2[ts1_sample, :])
 
-        # in_sample_var_list.append(in_sample_var / len(tree_set1))
-        # between_sample_var_list.append(between_sample_var / len(tree_set2))
-        # var_difference.append(abs((between_sample_var / len(tree_set2)) - (in_sample_var / len(tree_set1))))
-        psrf_like.append(np.sqrt((between_sample_var / len(tree_set2)) / (in_sample_var / len(tree_set1))))
 
-        # Same for a sample from the second tree set
-        ts2_sample = random.randint(0, len(tree_set2) - 1)
-        in_sample_var = np.sum(distances_pwts2[ts2_sample, :]) + np.sum(distances_pwts2[:, ts2_sample])
-        between_sample_var = np.sum(distance_pwts1ts2[:, ts2_sample])
-        psrf_like.append(np.sqrt((between_sample_var / len(tree_set1)) / (in_sample_var / len(tree_set2))))
-
-    # return var_difference, in_sample_var_list, between_sample_var_list, psrf_like
-    return psrf_like
+    return 0
