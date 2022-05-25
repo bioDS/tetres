2ed1cc2c0663c31dbf1e36edfc29fb93019eca17
Lars-B <berlinglars96@gmail.com>
2022-05-26 11:06:39 +1200

    adding test for cMChain class

--- tests/test_judgment/test_cmchain.py
+++ tests/test_judgment/test_cmchain.py
@@ -1,6 +1,7 @@
 import pytest
 from pathlib import Path
 from treeoclock.judgment.mchain import coupled_MChains
+import os
 
 
 def test_coupledMChain():
@@ -29,8 +30,30 @@ def test_cMChain_gelman_rubin_plot_single_chain():
         coupled_chains.gelman_rubin_like_diagnostic_plot()
 
 
-def test_cMChain_gelman_rubin_plot(ten_taxa_cMChain):
-    assert ten_taxa_cMChain.gelman_rubin_like_diagnostic_plot(), "Gelman Rubin Plot failed!"
+def test_cMChain_pwd_matrix(ten_taxa_cMChain):
+    errors = []
+    dm0 = ten_taxa_cMChain.pwd_matrix(0)
+    if dm0.shape != (1001, 1001):
+        errors.append("dm0 wrong shape!")
+    dm1 = ten_taxa_cMChain.pwd_matrix(1)
+    if dm1.shape != (1001, 1001):
+        errors.append("dm0 wrong shape!")
+    dm01 = ten_taxa_cMChain.pwd_matrix(0, 1)
+    if dm01.shape != (1001, 1001):
+        errors.append("dm0 wrong shape!")
+    assert errors == [], f"PWD matrix went wrong! {errors}"
 
+
+def test_cMChain_pwdm_errors(ten_taxa_cMChain):
+    with pytest.raises(ValueError):
+        for i in ["HI", 1.0, 2]:
+            ten_taxa_cMChain.pwd_matrix(i)
+            ten_taxa_cMChain.pwd_matrix(0, i)
+        ten_taxa_cMChain.pw_matrix(0, 0)
+
+
+def test_cMChain_gelman_rubin_plot(ten_taxa_cMChain):
+    ten_taxa_cMChain.gelman_rubin_like_diagnostic_plot()
+    assert os.path.exists(f"{ten_taxa_cMChain.working_dir}/{ten_taxa_cMChain.name}_grd_plot.png"), "Gelman Rubin Plot failed!"
 
 
