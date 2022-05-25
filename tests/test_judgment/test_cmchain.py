c1f2f2bf95326bf81ebe1c7c650b64bad443c94f
Lars-B <berlinglars96@gmail.com>
2022-05-26 10:29:01 +1200

    adding new data for cMChain tests

--- tests/test_judgment/test_cmchain.py
+++ tests/test_judgment/test_cmchain.py
@@ -8,7 +8,15 @@ def test_coupledMChain():
                            trees=["30Taxa.trees"],
                            log_files=["30Taxa_beast2.log"],
                            working_dir= f"{Path(__file__).parent.parent.absolute()}/data"
-                           ), "Construction coupledMChain with files failed!"
+                           ), "Construction coupledMChain 1 with files failed!"
+
+
+def test_coupledMChain():
+    assert coupled_MChains(m_MChains=2,
+                           trees=["chain0.trees", "chain0_1.trees"],
+                           log_files=["chain0.log", "chain0_1.log"],
+                           working_dir= f"{Path(__file__).parent.parent.absolute()}/data/cMChain"
+                           ), "Construction coupledMChain 2 with files failed!"
 
 
 def test_cMChain_gelman_rubin_plot_single_chain():
