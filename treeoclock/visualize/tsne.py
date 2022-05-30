5f6555de058b618dc1c409e429f1b5da196a38b4
Lars-B <berlinglars96@gmail.com>
2022-05-31 11:31:05 +1200

    adding tsne coordinate calculation for mchain object

---
+++ treeoclock/visualize/tsne.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sklearn import manifold
+import os
+
+
+def _tsne_coords_from_pwd(pwd_matrix, dim=2):
+    mds = manifold.TSNE(n_components=dim, metric="precomputed", learning_rate='auto')
+    coords = mds.fit_transform(pwd_matrix)
+    return coords
+
+
+def tsne_coords_from_mchain(mchain, dim=2):
+    # computing or loading the tsne coordinates
+    if not os.path.exists(f"{mchain.working_dir}/{mchain.name}_tsne_{dim}d_coords.npy"):
+        pwd_matrix = mchain.pwd_matrix()
+        coords = _tsne_coords_from_pwd(pwd_matrix=pwd_matrix, dim=dim)
+        np.save(file=f"{mchain.working_dir}/{mchain.name}_tsne_{dim}d_coords.npy", arr=coords)
+        return coords
+    else:
+        return np.load(f"{mchain.working_dir}/{mchain.name}_tsne_{dim}d_coords.npy")
