6d7cdeef66d089799585541e333173f71330b6b8
Lars-B <berlinglars96@gmail.com>
2022-06-07 11:23:45 +1200

    adding tests for exceptions and default working_dir=None

--- treeoclock/beast_helper/beter.py
+++ treeoclock/beast_helper/beter.py
@@ -10,12 +10,11 @@ def _init_R(func):
 
 
 @_init_R
-def process_template(xml_template, out_file, config, working_dir):
+def process_template(xml_template, out_file, config, working_dir=None):
     beter = importr("beter")
     if working_dir is None:
         if not os.path.exists(xml_template):
             raise FileNotFoundError(f"XML template {xml_template} does not exist!")
-
         if not os.path.exists(config):
             raise FileNotFoundError(f"Config file {config} does not exist!")
         beter.process_template(f"{xml_template}", f"{out_file}", f"{config}")
