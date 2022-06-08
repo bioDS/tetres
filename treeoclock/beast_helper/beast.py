061ed22bc723acaee9d38340b7f6892d2efd4b26
Lars-B <berlinglars96@gmail.com>
2022-06-09 08:45:51 +1200

    bugfix: resetting working directory after running beast

--- treeoclock/beast_helper/beast.py
+++ treeoclock/beast_helper/beast.py
@@ -7,7 +7,7 @@ def run_beast(xml_file, beast, chains=4):
         raise ValueError("Path to beast does not exist!")
     if type(chains) is not int or chains < 0:
         raise ValueError("Chains need to be an int > 0!")
-
+    cwd = os.getcwd()  # Getting current Working directory
     for i in range(1, chains+1):
         try:
             os.mkdir(f"{os.path.dirname(xml_file)}/run{i}")
@@ -15,5 +15,5 @@ def run_beast(xml_file, beast, chains=4):
             sys.exit(f"Folder run{i} already exists, won't overwrite!")
         os.chdir(f"{os.path.dirname(xml_file)}/run{i}")
         os.system(f"{beast} -beagle_cpu -overwrite -threads -1 {xml_file} > {os.path.dirname(xml_file)}/run{i}/command_output.txt 2>&1")
-
+    os.chdir(cwd)  # Resetting the working directory
     return 0
