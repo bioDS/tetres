f40994ea067c1a3ec4ea566377a5cf48bf4897ac
Lars-B <berlinglars96@gmail.com>
2022-06-08 14:22:43 +1200

    adding chain parameter for generating multiple independant chains

--- tests/test_beast_helper/test_beast.py
+++ tests/test_beast_helper/test_beast.py
@@ -4,7 +4,8 @@ import os
 
 
 def test_run_beast():
+    # todo this test fails after first run because the folder will be created, think of a better test?
     run_beast(xml_file=f"{Path(__file__).parent.parent.absolute()}/data/beast_helper/DS1.xml",
               beast="/home/lars/.local/share/beast/bin/beast")
-    assert os.path.exists(f"{Path(__file__).parent.parent.absolute()}/data/beast_helper/DS1.trees"),\
+    assert os.path.exists(f"{Path(__file__).parent.parent.absolute()}/data/beast_helper/run4/DS1.trees"),\
         "run_beast failed!"
--- treeoclock/beast_helper/beast.py
+++ treeoclock/beast_helper/beast.py
@@ -1,9 +1,19 @@
 import os
+import sys
 
 
-def run_beast(xml_file, beast):
+def run_beast(xml_file, beast, chains=4):
     if not os.path.exists(beast):
         raise ValueError("Path to beast does not exist!")
-    # os.chdir(os.path.dirname(xml_file))
-    os.system(f"{beast} -beagle_cpu -working -overwrite -threads -1 {xml_file} > {os.path.dirname(xml_file)}/command_output.txt 2>&1")
+    if type(chains) is not int or chains < 0:
+        raise ValueError("Chains need to be an int > 0!")
+
+    for i in range(1, chains+1):
+        try:
+            os.mkdir(f"{os.path.dirname(xml_file)}/run{i}")
+        except FileExistsError:
+            sys.exit(f"Folder run{i} already exists, won't overwrite!")
+        os.chdir(f"{os.path.dirname(xml_file)}/run{i}")
+        os.system(f"{beast} -beagle_cpu -overwrite -threads -1 {xml_file} > {os.path.dirname(xml_file)}/run{i}/command_output.txt 2>&1")
+
     return 0
