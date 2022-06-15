2009640dec59a69cac4861e2ef80654e107782d0
Lars-B <berlinglars96@gmail.com>
2022-06-15 14:14:00 +1200

    adding basic funciton to plot multiple chains in one, putting together a distance matrix, still WIP and missing some features

--- tests/test_visualize/test_plot_coords.py
+++ tests/test_visualize/test_plot_coords.py
@@ -1,9 +1,11 @@
 from treeoclock.visualize.tsne import tsne_coords_from_mchain
-from treeoclock.visualize.plot_coords import plot_coords, plot_density_over_coordinates
+from treeoclock.visualize.plot_coords import plot_coords, plot_density_over_coordinates, plot_multiple_chains_tsne
 
 import numpy as np
 
 
+# todo colors as keyword for log_data and generally the function should be called from the class
+#  instead of directly calling the function
 def test_plot_coords_2d(ten_taxa_cMChain):
     coords = tsne_coords_from_mchain(ten_taxa_cMChain[1], dim=2)
     plot_coords(coords, colors=np.asarray(ten_taxa_cMChain[1].log_data["likelihood"]))
@@ -18,3 +20,8 @@ def test_plot_density(ten_taxa_cMChain):
     coords = tsne_coords_from_mchain(ten_taxa_cMChain[1], dim=2)
     plot_density_over_coordinates(coords, np.asarray(ten_taxa_cMChain[1].log_data["posterior"]),
                                   filename=f"{ten_taxa_cMChain.working_dir}/testing/")
+
+
+def test_plot_multiple_chains_tsne(ten_taxa_cMChain):
+    plot_multiple_chains_tsne(ten_taxa_cMChain)
+    assert True
--- treeoclock/visualize/plot_coords.py
+++ treeoclock/visualize/plot_coords.py
@@ -11,15 +11,20 @@ def plot_coords(coords, filename=None, colors=None):
     # Normalizing the colors
     colors = [(c - np.min(colors)) / (np.max(colors) - np.min(colors)) for c in colors]
     cmap = plt.cm.Spectral_r
+    colors = [cmap(c) for c in colors]
+
+    # todo s is the size, so it would be possible to scale based on the colors
+    #  or also another value
+    s =[5 for _ in range(coords.shape[0])]
 
     if coords.shape[1] == 2:
         x, y = zip(*coords)
-        plt.scatter(x, y, c=[cmap(c) for c in colors])
+        plt.scatter(x, y, c=colors, s=s)
     elif coords.shape[1] == 3:
         x, y, z = zip(*coords)
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(x, y, z, c=[cmap(c) for c in colors])
+        ax.scatter(x, y, z, c=colors, s=s)
     else:
         raise ValueError(f"Not supported number of dimenstions to plot {coords.shape[1]}!")
 
@@ -37,11 +42,12 @@ def plot_density_over_coordinates(coords, density, filename):
     if coords.shape[1] != 2:
         raise ValueError(f"Wrong dimenstion coordinates given {coords.shape[1]}!")
     if density.shape[0] != coords.shape[0]:
-        raise ValueError(f"Wrong dimenstion density given, does not fit coords {density.shape[0]}!")
+        raise ValueError(f"Wrong dimenstion density given, does not fit coords {density.shape[0]}\{coords.shape[0]}!")
 
     x, y = zip(*coords)
     z = density
     z = [(c - np.min(z)) / (np.max(z) - np.min(z)) for c in z]
+    # todo there may be a problem with this scaling ?
 
     from scipy.interpolate import griddata
 
@@ -60,20 +66,25 @@ def plot_density_over_coordinates(coords, density, filename):
     ax.plot_surface(xi, yi, zi, rstride=1, cstride=1, cmap=plt.cm.Spectral_r)
     # ax.plot_wireframe(xi, yi, zi, rstride=1, cstride=1, cmap="autumn")
 
-    ax.xaxis.pane.fill = False
-    ax.xaxis.pane.set_edgecolor('white')
-    ax.yaxis.pane.fill = False
-    ax.yaxis.pane.set_edgecolor('white')
-    ax.zaxis.pane.fill = False
-    ax.zaxis.pane.set_edgecolor('white')
-    ax.grid(False)
-    ax.w_zaxis.line.set_lw(0.)
-    ax.set_zticks([])
+    plt.savefig(filename, format="png", dpi=400)
 
-    start = 45
-    for i in range(8):
-        ax.view_init(elev=15., azim=start*i)
-        plt.savefig(f"{filename}/{i}.png", format="png", dpi=400)
+
+    # ax.xaxis.pane.fill = False
+    # ax.xaxis.pane.set_edgecolor('white')
+    # ax.yaxis.pane.fill = False
+    # ax.yaxis.pane.set_edgecolor('white')
+    # ax.zaxis.pane.fill = False
+    # ax.zaxis.pane.set_edgecolor('white')
+    # ax.grid(False)
+    # ax.w_zaxis.line.set_lw(0.)
+    # ax.set_zticks([])
+    #
+    # start = 45
+    # for i in range(8):
+    #     ax.view_init(elev=15., azim=start*i)
+    #     plt.savefig(f"{filename}/{i}.png", format="png", dpi=400)
+
+
     # plt.show()
 
     # from matplotlib import animation
@@ -96,5 +107,63 @@ def plot_density_over_coordinates(coords, density, filename):
     # plt.rcParams['animation.ffmpeg_path'] = '/home/lars/mambaforge/bin/ffmpeg'
     # anim.save(filename, writer="ffmpeg", extra_args=['-vcodec', 'libx264'])
 
-
+    plt.clf()
     return 0
+
+
+def plot_multiple_chains_tsne(mchain):
+
+    # todo build new pw distance matrix from existing matrices
+    combined_matrix = np.array([])
+    colors = []
+    for i in range(mchain.m_MChains):
+        colors.extend([i for _ in range(len(mchain[i].trees))])
+        cur_row = np.array([])
+        for j in range(mchain.m_MChains):
+            print(i, j)
+            if i < j:
+                cur_row = np.concatenate((cur_row, mchain.pwd_matrix(i, j)), axis=1) if cur_row.size else mchain.pwd_matrix(i, j)
+            elif i > j:
+                cur_row = np.concatenate((cur_row, np.zeros(mchain.pwd_matrix(j, i).shape)), axis=1) if cur_row.size else np.zeros(mchain.pwd_matrix(j, i).shape)
+            elif i == j:
+                cur_row = np.concatenate((cur_row, mchain.pwd_matrix(i)), axis=1) if cur_row.size else mchain.pwd_matrix(i)
+            print(cur_row.shape)
+        combined_matrix = np.concatenate((combined_matrix, cur_row)) if combined_matrix.size else cur_row
+        # print(combined_matrix.shape)
+
+    # todo save the combined distance matrix!
+
+    # calling tsne for coordinate computaion
+    # todo ideally this should be outsorced and possible with tsne recognizing the input differently maybe
+    from treeoclock.visualize.tsne import _tsne_coords_from_pwd
+    coords = _tsne_coords_from_pwd(pwd_matrix=combined_matrix, dim=2)  # todo dimension
+
+    # continue with the rest and color the dots accordingly
+
+
+    # cmap = plt.cm.Spectral_r
+    # colors = [cmap(c) for c in colors]
+
+    # todo s is the size, so it would be possible to scale based on the colors
+    #  or also another value
+    s =[5 for _ in range(coords.shape[0])]
+
+    if coords.shape[1] == 2:
+        x, y = zip(*coords)
+        plt.scatter(x, y, c=colors, s=s)
+    elif coords.shape[1] == 3:
+        x, y, z = zip(*coords)
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(x, y, z, c=colors, s=s)
+    else:
+        raise ValueError(f"Not supported number of dimenstions to plot {coords.shape[1]}!")
+
+    # plt.colorbar(plt.cm.ScalarMappable(norm=None, cmap=cmap), orientation="horizontal")
+
+    plt.show()
+    # if filename is None:
+    #     plt.show()
+    # else:
+    #     plt.savefig(filename, dpi=400, format="png")
+    plt.clf()
