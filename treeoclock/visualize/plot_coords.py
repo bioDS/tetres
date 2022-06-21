033e60a7948dd18e66f86a8094040d76c21755dc
Lars-B <berlinglars96@gmail.com>
2022-06-21 16:06:15 +1200

    adding multiple chain plot using isomap (temporary function), lots of todos, basic funciton to generate gif for the 3d plot

--- treeoclock/visualize/plot_coords.py
+++ treeoclock/visualize/plot_coords.py
@@ -1,21 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation, PillowWriter
+
 
 # todo in the future a cluster parameter will be added
 # todo in the future likely a center parameter will be added to visualize summary trees
 # todo in the future maybe adding a dimension parameter ?
-import matplotlib.pyplot as plt
-import numpy as np
-
-
 def plot_coords(coords, filename=None, colors=None):
-
     # Normalizing the colors
-    colors = [(c - np.min(colors)) / (np.max(colors) - np.min(colors)) for c in colors]
+    if colors is not None:
+        colors = [(c - np.min(colors)) / (np.max(colors) - np.min(colors)) for c in colors]
+    else:
+        colors = [1 for _ in range(coords.shape[0])]
     cmap = plt.cm.Spectral_r
     colors = [cmap(c) for c in colors]
-
+    
     # todo s is the size, so it would be possible to scale based on the colors
     #  or also another value
-    s =[5 for _ in range(coords.shape[0])]
+    s = [5 for _ in range(coords.shape[0])]
 
     if coords.shape[1] == 2:
         x, y = zip(*coords)
@@ -28,7 +30,8 @@ def plot_coords(coords, filename=None, colors=None):
     else:
         raise ValueError(f"Not supported number of dimenstions to plot {coords.shape[1]}!")
 
-    plt.colorbar(plt.cm.ScalarMappable(norm=None, cmap=cmap), orientation="horizontal")
+    if colors is not None:
+        plt.colorbar(plt.cm.ScalarMappable(norm=None, cmap=cmap), orientation="horizontal")
 
     if filename is None:
         plt.show()
@@ -38,7 +41,6 @@ def plot_coords(coords, filename=None, colors=None):
 
 
 def plot_density_over_coordinates(coords, density, filename):
-
     if coords.shape[1] != 2:
         raise ValueError(f"Wrong dimenstion coordinates given {coords.shape[1]}!")
     if density.shape[0] != coords.shape[0]:
@@ -56,97 +58,73 @@ def plot_density_over_coordinates(coords, density, filename):
 
     xi, yi = np.mgrid[np.min(x):np.max(x):1000j, np.min(y):np.max(y):1000j]
 
-
     zi = griddata(points=(x, y), values=z, xi=(xi, yi), method='linear', fill_value=np.min(z))
-
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     # ax.scatter(x, y, z)
-    ax.plot_surface(xi, yi, zi, rstride=1, cstride=1, cmap=plt.cm.Spectral_r)
-    # ax.plot_wireframe(xi, yi, zi, rstride=1, cstride=1, cmap="autumn")
+    dens = ax.plot_surface(xi, yi, zi, rstride=1, cstride=1, cmap=plt.cm.Spectral_r)
 
-    plt.savefig(filename, format="png", dpi=400)
+    def init():
+        ax.view_init(elev=10., azim=0)
+        return [dens]
 
+    def animate(i):
+        ax.view_init(elev=15., azim=i * 45)
+        return [dens]
 
-    # ax.xaxis.pane.fill = False
-    # ax.xaxis.pane.set_edgecolor('white')
-    # ax.yaxis.pane.fill = False
-    # ax.yaxis.pane.set_edgecolor('white')
-    # ax.zaxis.pane.fill = False
-    # ax.zaxis.pane.set_edgecolor('white')
-    # ax.grid(False)
-    # ax.w_zaxis.line.set_lw(0.)
-    # ax.set_zticks([])
-    #
-    # start = 45
-    # for i in range(8):
-    #     ax.view_init(elev=15., azim=start*i)
-    #     plt.savefig(f"{filename}/{i}.png", format="png", dpi=400)
-
-
-    # plt.show()
-
-    # from matplotlib import animation
-    #
-    # ax.plot_surface(xi, yi, zi, rstride=1, cstride=1, cmap=plt.cm.Spectral_r)
-    #
-    # def init():
-    #     # ax.scatter(xx, yy, zz, marker='o', s=20, c="goldenrod", alpha=0.6)
-    #     ax.plot_surface(xi, yi, zi, rstride=1, cstride=1, cmap=plt.cm.Spectral_r)
-    #     return fig,
-    #
-    # def animate(i):
-    #     ax.view_init(elev=15., azim=i)
-    #     return fig,
-    #
-    # # Animate
-    # anim = animation.FuncAnimation(fig, animate, init_func=init, frames=360,
-    #                                blit=True)
-    # Save
-    # plt.rcParams['animation.ffmpeg_path'] = '/home/lars/mambaforge/bin/ffmpeg'
-    # anim.save(filename, writer="ffmpeg", extra_args=['-vcodec', 'libx264'])
+    anim = FuncAnimation(fig, animate, init_func=init,
+                         frames=8, interval=1000, blit=True)
+    # # Save
+    writergif = PillowWriter(fps=30)
+    anim.save(f'{filename}/distribution.gif', writer=writergif)
 
     plt.clf()
     return 0
 
 
-def plot_multiple_chains_tsne(mchain):
+def plot_multiple_chains_tsne(mchain, names):
+    # names = [f"chain{i}" for i in range(coupled_chains.m_MChains)])  # todo this should be default names
 
-    # todo build new pw distance matrix from existing matrices
+    # labels = []
     combined_matrix = np.array([])
+    cmap = plt.cm.Set1
     colors = []
     for i in range(mchain.m_MChains):
-        colors.extend([i for _ in range(len(mchain[i].trees))])
+        colors.extend([cmap(i) for _ in range(len(mchain[i].trees))])
+        # labels.append([names[i] for _ in range(len(mchain[i].trees))])
         cur_row = np.array([])
         for j in range(mchain.m_MChains):
-            print(i, j)
+            # print(i, j)
             if i < j:
-                cur_row = np.concatenate((cur_row, mchain.pwd_matrix(i, j)), axis=1) if cur_row.size else mchain.pwd_matrix(i, j)
+                cur_row = np.concatenate((cur_row, mchain.pwd_matrix(i, j)),
+                                         axis=1) if cur_row.size else mchain.pwd_matrix(i, j)
             elif i > j:
-                cur_row = np.concatenate((cur_row, np.zeros(mchain.pwd_matrix(j, i).shape)), axis=1) if cur_row.size else np.zeros(mchain.pwd_matrix(j, i).shape)
+                cur_row = np.concatenate((cur_row, np.zeros(mchain.pwd_matrix(j, i).shape)),
+                                         axis=1) if cur_row.size else np.zeros(mchain.pwd_matrix(j, i).shape)
             elif i == j:
-                cur_row = np.concatenate((cur_row, mchain.pwd_matrix(i)), axis=1) if cur_row.size else mchain.pwd_matrix(i)
-            print(cur_row.shape)
+                cur_row = np.concatenate((cur_row, mchain.pwd_matrix(i)),
+                                         axis=1) if cur_row.size else mchain.pwd_matrix(i)
+            # print(cur_row.shape)
         combined_matrix = np.concatenate((combined_matrix, cur_row)) if combined_matrix.size else cur_row
         # print(combined_matrix.shape)
-
     # todo save the combined distance matrix!
-
+    # todo make other visualization methods possible
+    #  like metric MDS ! 
     # calling tsne for coordinate computaion
     # todo ideally this should be outsorced and possible with tsne recognizing the input differently maybe
     from treeoclock.visualize.tsne import _tsne_coords_from_pwd
+    # todo it is totally fine to run tsne 10 times and select the solutin with the lowest KL divergence
     coords = _tsne_coords_from_pwd(pwd_matrix=combined_matrix, dim=2)  # todo dimension
 
     # continue with the rest and color the dots accordingly
-
 
     # cmap = plt.cm.Spectral_r
     # colors = [cmap(c) for c in colors]
 
     # todo s is the size, so it would be possible to scale based on the colors
     #  or also another value
-    s =[5 for _ in range(coords.shape[0])]
+    s = [5 for _ in range(coords.shape[0])]
 
     if coords.shape[1] == 2:
         x, y = zip(*coords)
@@ -160,10 +138,88 @@ def plot_multiple_chains_tsne(mchain):
         raise ValueError(f"Not supported number of dimenstions to plot {coords.shape[1]}!")
 
     # plt.colorbar(plt.cm.ScalarMappable(norm=None, cmap=cmap), orientation="horizontal")
+    # plt.legend([cmap(i) for i in ], names)
 
-    plt.show()
+    markers = [plt.Line2D([0, 0], [0, 0], color=cmap(i), marker='o', linestyle='') for i in range(mchain.m_MChains)]
+    plt.legend(markers, names, numpoints=1)
+
+    plt.savefig(f"{mchain.working_dir}/plots/{mchain.name}_tsne_all_chains.png", dpi=400, bbox_inches="tight")
+    # plt.show()
     # if filename is None:
     #     plt.show()
     # else:
     #     plt.savefig(filename, dpi=400, format="png")
     plt.clf()
+    plt.close()
+
+
+# todo temporary funciton, to be merged with above which will allow different coordinates/ mds methods
+def plot_multiple_chains_isomap(mchain, names):
+    # names = [f"chain{i}" for i in range(coupled_chains.m_MChains)])  # todo this should be default names
+
+    # labels = []
+    combined_matrix = np.array([])
+    cmap = plt.cm.Set1
+    colors = []
+    for i in range(mchain.m_MChains):
+        colors.extend([cmap(i) for _ in range(len(mchain[i].trees))])
+        # labels.append([names[i] for _ in range(len(mchain[i].trees))])
+        cur_row = np.array([])
+        for j in range(mchain.m_MChains):
+            # print(i, j)
+            if i < j:
+                cur_row = np.concatenate((cur_row, mchain.pwd_matrix(i, j)),
+                                         axis=1) if cur_row.size else mchain.pwd_matrix(i, j)
+            elif i > j:
+                cur_row = np.concatenate((cur_row, np.zeros(mchain.pwd_matrix(j, i).shape)),
+                                         axis=1) if cur_row.size else np.zeros(mchain.pwd_matrix(j, i).shape)
+            elif i == j:
+                cur_row = np.concatenate((cur_row, mchain.pwd_matrix(i)),
+                                         axis=1) if cur_row.size else mchain.pwd_matrix(i)
+            # print(cur_row.shape)
+        combined_matrix = np.concatenate((combined_matrix, cur_row)) if combined_matrix.size else cur_row
+        # print(combined_matrix.shape)
+    # todo save the combined distance matrix!
+    # todo make other visualization methods possible
+    #  like metric MDS !
+    # todo ideally this should be outsorced and possible with tsne recognizing the input differently maybe
+
+    from sklearn import manifold
+    imap = manifold.Isomap(n_components=2, metric="precomputed")
+    combined_matrix = combined_matrix + combined_matrix.transpose()
+    coords = imap.fit_transform(combined_matrix)
+
+    # continue with the rest and color the dots accordingly
+
+    # cmap = plt.cm.Spectral_r
+    # colors = [cmap(c) for c in colors]
+
+    # todo s is the size, so it would be possible to scale based on the colors
+    #  or also another value
+    s = [5 for _ in range(coords.shape[0])]
+
+    if coords.shape[1] == 2:
+        x, y = zip(*coords)
+        plt.scatter(x, y, c=colors, s=s)
+    elif coords.shape[1] == 3:
+        x, y, z = zip(*coords)
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(x, y, z, c=colors, s=s)
+    else:
+        raise ValueError(f"Not supported number of dimenstions to plot {coords.shape[1]}!")
+
+    # plt.colorbar(plt.cm.ScalarMappable(norm=None, cmap=cmap), orientation="horizontal")
+    # plt.legend([cmap(i) for i in ], names)
+
+    markers = [plt.Line2D([0, 0], [0, 0], color=cmap(i), marker='o', linestyle='') for i in range(mchain.m_MChains)]
+    plt.legend(markers, names, numpoints=1)
+
+    plt.savefig(f"{mchain.working_dir}/plots/{mchain.name}_isomap_all_chains.png", dpi=400, bbox_inches="tight")
+    # plt.show()
+    # if filename is None:
+    #     plt.show()
+    # else:
+    #     plt.savefig(filename, dpi=400, format="png")
+    plt.clf()
+    plt.close()
