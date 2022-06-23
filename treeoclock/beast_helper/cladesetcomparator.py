0809b3f8d8c153947e9ae44e483ed2240fec1bb6
Lars-B <berlinglars96@gmail.com>
2022-06-23 12:00:38 +1200

    Adding cladesetcomparator.py beast babel app

---
+++ treeoclock/beast_helper/cladesetcomparator.py
@@ -0,0 +1,13 @@
+import os
+
+
+def run_cladesetcomparator(tree_file1, tree_file2, out_file_png, beast_applauncher):
+    if not os.path.exists(beast_applauncher):
+        raise ValueError("Path to beast-applauncher does not exist!")
+
+    os.system(
+        f"{beast_applauncher} CladeSetComparator "
+        f"-tree1 {tree_file1} "
+        f"-tree2 {tree_file2} "
+        f"-png {out_file_png} "
+        f"> {out_file_png}.txt")
