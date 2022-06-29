b2f3c7ef877b562e08aa79286537b586d364085b
Lars-B <berlinglars96@gmail.com>
2022-06-29 13:50:52 +1200

    adding transpose to make symmetry matrix symmetrical

--- treeoclock/judgment/_plotting.py
+++ treeoclock/judgment/_plotting.py
@@ -11,6 +11,7 @@ def all_chains_spectral_clustree(cmchain, beta=1, n_clus=2):
     # todo only if c is not avaialable, i.e. add saving for clustering
     if not os.path.exists(f"{cmchain.working_dir}/data/{cmchain.name}_{'' if beta == 1 else f'{beta}_'}{n_clus}clustering_all.npy"):
         similarity = cmchain.similarity_matrix_all(beta=beta)
+        similarity = similarity + similarity.transpose()
         clustering = _spectral_clustree(similarity, n_clus=n_clus)
         np.save(file=f"{cmchain.working_dir}/data/{cmchain.name}_{'' if beta == 1 else f'{beta}_'}{n_clus}clustering_all.npy",
                 arr=clustering)
