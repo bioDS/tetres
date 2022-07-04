3d33e7a2de0275083c0511d924ff4309a809d31e
Lars-B <berlinglars96@gmail.com>
2022-07-04 15:54:00 +1200

    bugfix: adding shared numpy arrays

--- treeoclock/judgment/_pairwise_distance_matrix.py
+++ treeoclock/judgment/_pairwise_distance_matrix.py
@@ -4,17 +4,18 @@ import os
 import numpy as np
 from multiprocessing import Pool
 import itertools
+from share_array.share_array import get_shared_array, make_shared_array
 
 
 lib = CDLL(f"{os.path.dirname(os.path.dirname(os.path.realpath(__file__)))}/trees/findpath.so")
 
 
-def _rf(t1, t2, i, j, matrix):
+def _rf(t1, t2, i, j):
+    matrix = get_shared_array('distances')
     matrix[i, j] = t1.robinson_foulds(t2)[0]
     return 0
 
 
-# todo testing required!
 def calc_pw_distances(trees, rf: bool = False):
     # todo add a n_cores parameter to c function?
     n = len(trees)
@@ -27,12 +28,13 @@ def calc_pw_distances(trees, rf: bool = False):
     else:
         distances = np.zeros((n, n), dtype=c_long)
         etrees = [t.etree for t in trees]
+        make_shared_array(distances, name='distances')  # create shared memory array from numpy array
+        # shared_array = get_shared_array('distances')  # get shared memory array as numpy array
         with Pool(None) as p:
-            p.starmap(_rf, [(etrees[i], etrees[j], i, j, distances) for i,j in itertools.combinations(range(n), 2)])
-        return distances
+            p.starmap(_rf, [(etrees[i], etrees[j], i, j) for i,j in itertools.combinations(range(n), 2)])
+        return get_shared_array('distances')
 
 
-# todo testing required
 def calc_pw_distances_two_sets(trees1, trees2, rf: bool = False):
     # todo add a n_cores parameter to c function?
     if len(trees1) != len(trees2):
@@ -49,8 +51,8 @@ def calc_pw_distances_two_sets(trees1, trees2, rf: bool = False):
         distances = np.zeros((n, n), dtype=c_long)
         etrees1 = [t.etree for t in trees1]
         etrees2 = [t.etree for t in trees2]
-
+        make_shared_array(distances, name='distances')  # create shared memory array from numpy array
         with Pool(None) as p:
-            p.starmap(_rf, [(etrees1[i], etrees2[j], i, j, distances) for i, j in
+            p.starmap(_rf, [(etrees1[i], etrees2[j], i, j) for i, j in
                             itertools.product(range(n), repeat=2)])
-        return distances
+        return get_shared_array('distances')
