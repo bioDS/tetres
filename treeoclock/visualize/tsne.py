4a02e79bb4dc2dc2349027b899d8355ba15f5df3
Lars-B <berlinglars96@gmail.com>
2022-07-13 14:13:46 +1200

    changing back tsne function

--- treeoclock/visualize/tsne.py
+++ treeoclock/visualize/tsne.py
@@ -5,16 +5,22 @@ import os
 
 def _tsne_coords_from_pwd(pwd_matrix, dim=2):
     internal_matrix = pwd_matrix + pwd_matrix.transpose()
-    mds = manifold.TSNE(n_components=dim, metric="precomputed", learning_rate='auto')
+    mds = manifold.TSNE(n_components=dim,
+                        metric="precomputed",
+                        learning_rate='auto',
+                        init="random",
+                        perplexity=10)
     coords = mds.fit_transform(internal_matrix)
-    return coords
+    return coords, mds.kl_divergence_
 
 
 def tsne_coords_from_mchain(mchain, dim=2):
     # computing or loading the tsne coordinates
+    # todo maybe change this to do a recomputation if wanted and overwriting of the old if better fit
     if not os.path.exists(f"{mchain.working_dir}/data/{mchain.name}_tsne_{dim}d_coords.npy"):
         pwd_matrix = mchain.pwd_matrix()
-        coords = _tsne_coords_from_pwd(pwd_matrix=pwd_matrix, dim=dim)
+        coords, kl_divergence = _tsne_coords_from_pwd(pwd_matrix=pwd_matrix, dim=dim)
+        # todo do something with the kl_divergence value
         np.save(file=f"{mchain.working_dir}/data/{mchain.name}_tsne_{dim}d_coords.npy", arr=coords)
         return coords
     else:
