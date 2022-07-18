c397b57deacaf92bfb009c8723cb923d3f9e7592
Lars-B <berlinglars96@gmail.com>
2022-07-19 11:00:15 +1200

    adapting sos test for all trees in set

--- tests/test_summary/test_compute_sos.py
+++ tests/test_summary/test_compute_sos.py
@@ -6,7 +6,7 @@ from treeoclock.trees.time_trees import TimeTreeSet
 def test_compute_sos_one_core(dir, five_taxa_nexus_string):
     dir.write("test.nex", five_taxa_nexus_string)
     trees = TimeTreeSet(f'{dir.path}/test.nex')
-    assert compute_sos_mt(t=trees[0], trees=trees, n_cores=1) == 5, "Compute_sos_mp failed (1=cores), should be 5!"
+    assert [compute_sos_mt(t=ct, trees=trees, n_cores=1) for ct in trees] == [5,10,13], "Compute_sos_mp failed (1=cores), should be 5!"
 
 
 def test_compute_sos_none_core(dir, five_taxa_nexus_string):
