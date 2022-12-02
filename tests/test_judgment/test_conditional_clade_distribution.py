a2c5d9f727e20e21a3e712fcbb13f1c058854b57
Lars-B <berlinglars96@gmail.com>
2022-12-02 23:09:47 +1300

    Implementing Larget algorithm to calculate conditional clade distribution/ tree probabilities

---
+++ tests/test_judgment/test_conditional_clade_distribution.py
@@ -0,0 +1,32 @@
+from treeoclock.judgment.conditional_clade_distribution import get_maps, get_tree_probability
+from collections import Counter
+
+def test_get_maps(ten_taxa_cMChain):
+    m1, m2 = get_maps(ten_taxa_cMChain[0].trees)
+    assert True # todo not sure
+
+
+def test_get_tree_probability(ten_taxa_cMChain):
+    m1, m2 = get_maps(ten_taxa_cMChain[0].trees)
+    p = get_tree_probability(ten_taxa_cMChain[0].trees[1], m1, m2)
+    assert p == 0.0037693679383000523, "Get tree probability failed!"
+
+
+def test_coverage(ten_taxa_cMChain):
+    m1, m2 = get_maps(ten_taxa_cMChain[0].trees)
+    probs = []
+    for t in ten_taxa_cMChain[0].trees:
+        probs.append(get_tree_probability(t, m1, m2))
+    # The sum over the probabilities of all unique trees should be 1 (if all of the distribution or the whole space has been sampled)
+    assert sum(Counter(probs)) == 1.0, "Coverage sum failed"
+
+
+def test_coverage2(twenty_taxa_tts):
+    m1, m2 = get_maps(twenty_taxa_tts)
+    probs = []
+    for t in twenty_taxa_tts:
+        probs.append(get_tree_probability(t, m1, m2))
+    # The sum over the probabilities of all unique trees should be 1 (if all of the distribution or the whole space has been sampled)
+    assert sum(Counter(probs)) == 0.6749181338892793, "Coverage sum failed"
+
+
---
+++ treeoclock/judgment/conditional_clade_distribution.py
@@ -0,0 +1,54 @@
+# algorithm for condiditonal clade distrbitution by Bret Larget
+from collections import defaultdict
+
+
+def get_maps(trees):
+
+    m1 = defaultdict(int)  # map for each clade how often it got sampled
+    m2 = defaultdict(int)  # map for each (c1,c2) clade how often this specific relation got sampled
+
+    # todo m1 needs to have the root and the single leaf clades
+    #  need to get a list of weights for the unique trees
+    #  this needs to replace trees in the for loop and the weight will be added instead of 1 each time!
+
+    for t in trees:
+        for node in t.etree.traverse("levelorder"):
+            if len(node) > 2:
+                c = node.children
+                c0_leafs = set()
+                for leaf in c[0]:
+                    c0_leafs.add(int(leaf.name))
+                c1_leafs = set()
+                for leaf in c[1]:
+                    c1_leafs.add(int(leaf.name))
+                parent_clade = frozenset(sorted(c0_leafs.union(c1_leafs)))
+
+                m1[parent_clade] += 1
+                if min(c0_leafs) < min(c1_leafs):
+                    m2[(parent_clade, frozenset(c0_leafs))] += 1
+                else:
+                    m2[(parent_clade, frozenset(c1_leafs))] += 1
+    return m1, m2
+
+
+def get_tree_probability(tree, m1, m2):
+    probability = 1
+    for node in tree.etree.traverse("levelorder"):
+        if len(node) > 2:
+            c = node.children
+            c0_leafs = set()
+            for leaf in c[0]:
+                c0_leafs.add(int(leaf.name))
+            c1_leafs = set()
+            for leaf in c[1]:
+                c1_leafs.add(int(leaf.name))
+            parent_clade = frozenset(sorted(c0_leafs.union(c1_leafs)))
+            if m1[parent_clade] != 0:
+                if min(c0_leafs) < min(c1_leafs):
+                    probability *= m2[(parent_clade, frozenset(c0_leafs))] / m1[parent_clade]
+                else:
+                    probability *= m2[(parent_clade, frozenset(c1_leafs))] / m1[parent_clade]
+        if probability == 0:
+            return 0
+
+    return probability
