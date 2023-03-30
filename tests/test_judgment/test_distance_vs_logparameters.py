ece4eb9047f68e646689410b19ffc2a768094c2b
Lars-B <berlinglars96@gmail.com>
2023-03-31 08:59:49 +1300

    renaming to chain

--- tests/test_judgment/test_distance_vs_logparameters.py
+++ tests/test_judgment/test_distance_vs_logparameters.py
@@ -1,26 +1,22 @@
 from tetres.judgment.distance_vs_logparameters import *
-from tetres.judgment.distance_vs_logparameters import _kc
 
 
-# todo this testing is nothing that is useful
-
-
-def test_plot_distance_posterior_comparison(thirty_taxa_MChain):
-    plot_distance_posteroir_comparison(thirty_taxa_MChain)
+def test_plot_distance_posterior_comparison(thirty_taxa_chain):
+    plot_distance_posteroir_comparison(thirty_taxa_chain)
     assert True
 
 
-def test_plot_sos_log_comparison(thirty_taxa_MChain):
-    plot_sos_log_comparison(thirty_taxa_MChain)
+def test_plot_sos_log_comparison(thirty_taxa_chain):
+    plot_sos_log_comparison(thirty_taxa_chain)
     assert True
 
 
-def test_loglike_along_path(thirty_taxa_MChain):
-    plot_loglik_along_path(thirty_taxa_MChain)
+def test_loglike_along_path(thirty_taxa_chain):
+    plot_loglik_along_path(thirty_taxa_chain)
     assert True
 
 
-def test_plot_log_neighbours(thirty_taxa_MChain):
+def test_plot_log_neighbours(thirty_taxa_chain):
     for dist_type in ["kc"]:  # "rnni", "rf"]:
-        plot_log_neighbours(thirty_taxa_MChain, dist_type=dist_type)
+        plot_log_neighbours(thirty_taxa_chain, dist_type=dist_type, kc_param=0.5)
     assert True
