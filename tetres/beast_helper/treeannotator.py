ba1e9abb72405a28c796cacd46c91e77af2fa165
Lars-B <berlinglars96@gmail.com>
2023-05-15 10:44:56 +1200

    Change to use subprocess.run(), adding heights option

--- tetres/beast_helper/treeannotator.py
+++ tetres/beast_helper/treeannotator.py
@@ -1,13 +1,14 @@
 import os
+import subprocess
 
 
-def run_treeannotator(tree_file, outfile, beast_treeannotator, burnin=10):
+def run_treeannotator(tree_file, outfile, beast_treeannotator, burnin=10, heights="ca"):
     if not os.path.exists(beast_treeannotator):
         raise ValueError("Path to beast-applauncher does not exist!")
 
-    os.system(
-        f"{beast_treeannotator} "
-        f"-burnin {burnin} "
-        f"-heights 'ca' "
-        f"{tree_file} "
-        f"{outfile}")
+    cmd = [beast_treeannotator,
+           "-burnin", f"{burnin}",
+           "-heights", heights,
+           tree_file,
+           outfile]
+    subprocess.run(cmd, check=True)
