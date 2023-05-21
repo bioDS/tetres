9814b490006d70131733b7b0a0d288d876ae74c8
Lars-B <berlinglars96@gmail.com>
2023-05-22 09:21:19 +1200

    adding Remcos burnin detection algorithm, slightly modified

---
+++ tests/test_judgment/test_burnin_detection.py
@@ -0,0 +1,12 @@
+from tetres.judgment.burnin_detection import burn_detector
+import pytest
+
+
+def test_burn_detector(ten_taxa_multichain):
+    burn_detector(ten_taxa_multichain, 0, 1)
+    assert True
+
+
+def test_burn_detector_valueerror(ten_taxa_multichain):
+    with pytest.raises(ValueError):
+        burn_detector(ten_taxa_multichain, 0, 1, traces = ["posterior","blah"])
---
+++ tetres/judgment/burnin_detection.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+def burn_detector(multichain, chain_i, chain_j, traces=["posterior", "likelihood", "prior"]):
+    # Calculates the burnin that is common between the given chains i and j
+    # This is the maximum for i and j (could be indiviual but for convenience we take a common burnin)
+    is_log = [tr in multichain[chain_i].log_data for tr in traces]
+    if not all(is_log):
+        raise ValueError(f"Given trace indicator -{traces[is_log.index(False)]}- is not present in the logfile!")
+
+    burn_ins = []  # storing the burnin for each parameter
+    index75 = int(len(
+        multichain[chain_i].log_data[traces[0]].values) * 0.75)  # calculating the 75% index, assuming both chains, and all log-parameters have same length
+    M = 10  # smoothing parameter
+
+    for tr in traces:
+        cur_burn_i, cur_burn_j = index75, index75
+        mean_i = np.mean(multichain[chain_i].log_data[tr].values[index75:])
+        var_i = np.var(multichain[chain_i].log_data[tr].values[index75:])
+        mean_j = np.mean(multichain[chain_j].log_data[tr].values[index75:])
+        var_j = np.var(multichain[chain_j].log_data[tr].values[index75:])
+        i_set, j_set = False, False
+        for i in range(M, index75):
+            if not i_set:
+                if mean_i - var_i <= np.mean(multichain[chain_i].log_data[tr].values[i-M:i]) <= mean_i + var_i:
+                    cur_burn_i = i
+                    i_set = True
+            if not j_set:
+                if mean_j - var_j <= np.mean(multichain[chain_j].log_data[tr].values[i-M:i]) <= mean_j + var_j:
+                    cur_burn_j = i
+                    j_set = True
+            if i_set and j_set:
+                break
+        burn_ins.append(np.max((cur_burn_j, cur_burn_i)))
+    return np.max(burn_ins)
