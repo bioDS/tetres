26b3181443d2d957fe75a6d8e12cc4e81b94ecb9
Lars-B <berlinglars96@gmail.com>
2023-06-27 10:35:19 +1200

    adding new bottom up DP alg to find maximal CCD tree

--- tetres/partitions/conditional_clade_distribution.py
+++ tetres/partitions/conditional_clade_distribution.py
@@ -2,6 +2,7 @@
 from collections import defaultdict
 from decimal import *
 import ete3
+import math
 from tetres.trees.time_trees import TimeTree
 
 
@@ -66,6 +67,9 @@ def get_tree_probability(tree, m1, m2):
 
 def get_ccd_tree_branch_bound(m1, m2, prob):
 
+    # todo figure out why this returns the same tree multiple times
+    #  todo make some improvements to the efficiency, maybe add multiprocessing???
+
     working_list = [([max(m1.keys())], [], 1)]  # initialize with root clade, empty tree, probability 1
     output = []
     while working_list:
@@ -73,8 +77,11 @@ def get_ccd_tree_branch_bound(m1, m2, prob):
         cur_tuple = working_list.pop()
         for tuple_split in cur_tuple[0]:
             # iterate over all unresolved parts of the tree
+
             all_splits_of_cur_tuple = [(m2[p, c], c) for p, c in [(list(i)[0], list(i)[1]) for i in m2.keys()] if
                                         p == tuple_split]
+            # todo here we only need to look at splits once, because if we look at it a second time then it will end with the same result?!?!?!
+
             for split in all_splits_of_cur_tuple:
                 # iterate over all possible splits of this currently unresolved part of the tree
                 new_prob = cur_tuple[2] * (split[0] / m1[tuple_split])
@@ -92,15 +99,77 @@ def get_ccd_tree_branch_bound(m1, m2, prob):
                     if not new_list:
                         # if the new list does not contain anything we have a fully resolved tree
                         #  hence we add it to output and don't need to resolve it further
+
+                        # todo check that output does not contain the current element, i.e. make output a list?
                         output.append((tree_as_list_of_splits,
                                          new_prob))
+
                     else:
                         # current tree is not yet fully resolved, so add it to working list
+
+                        # todo same thing as above, check whether this new list is not already in there
+                        #  i.e. is the same form of tree already being worked on, if yes, abandon this branch instead
+                        # if tree_as_list_of_splits not in [i[1] for i in working_list]:
                         working_list.append((new_list,
-                                         tree_as_list_of_splits,
-                                         new_prob))
+                                             tree_as_list_of_splits,
+                                             new_prob))
     best_bb_tree = max(output, key=lambda item: item[1])
     return get_tree_from_list_of_splits(best_bb_tree[0]), output, best_bb_tree[1]
+
+
+def get_ccd_tree_dfs(m1, m2, prob):
+    # working_list = [([max(m1.keys())], [], 1)]  # initialize with root clade, empty tree, probability 1
+
+    useless = {}  # saving all clades that will not lead to a better tree
+    seen_resolved_clades = {}
+
+    all_clades = sorted(list(m1.keys()), key=len)
+    for current_clade in all_clades:
+        all_splits_current = [i for i in m2 if i[0] == current_clade]
+        for split in all_splits_current:
+            # this for loop needs to find the best split of the current parent, if any exists!
+            child1 = split[1]
+            child2 = split[0].difference(split[1])
+
+            if child1 in useless or child2 in useless:
+                # Current Split is not going to improve solution
+                continue
+
+            if len(child1) < 3:
+                # length 2 or 1 gives probability 1
+                c1_prob = 1
+            else:
+                if child1 in seen_resolved_clades:
+                    c1_prob, _ = seen_resolved_clades[child1]
+            if len(child2) < 3:
+                # length 2 or 1 gives probability 1
+                c2_prob = 1
+            else:
+                if child2 in seen_resolved_clades:
+                    c2_prob, _ = seen_resolved_clades[child2]
+
+            if (c1_prob * c2_prob) < prob:
+                # The current split will not lead to an improvement
+                continue
+
+            cur_prob = m2[split] / m1[split[0]]  # Prob of current parent, given split
+            split_prob = c1_prob * c2_prob * cur_prob  # best probability of current parent with split
+            if math.isclose(split_prob, prob) or split_prob > prob:
+            # if split_prob >= prob:  # does not work due to float comparison, 0.1 + 0.2 != 0.3, use math.isclose instead of ==
+                if split[0] in seen_resolved_clades:
+                    # parent was already found, do we need to update?
+                    if seen_resolved_clades[split[0]][0] <= split_prob:
+                        # this split has better probability, therefore update the seen_resolved_clades with the better split of split[0]
+                        seen_resolved_clades[split[0]] = (split_prob, child1)
+                else:
+                    # we have not seen the parent before
+                    seen_resolved_clades[split[0]] = (split_prob, child1)
+        if current_clade not in seen_resolved_clades:
+            # all possible splits of current_clade have not resulted in a tree with higher probability
+            #  therefore add it to the useless pile
+            useless[current_clade] = 0
+    # todo build a tree from the seen and resolved dict
+    return seen_resolved_clades
 
 
 def get_greedy_ccd_tree(m1, m2):
