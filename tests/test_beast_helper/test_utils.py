82178a4288a151330885be0fd31268a563ebeb22
Lars-B <berlinglars96@gmail.com>
2023-09-11 08:24:01 +1200

    fixing map problem, values should be strings

--- tests/test_beast_helper/test_utils.py
+++ tests/test_beast_helper/test_utils.py
@@ -6,7 +6,7 @@ from tetres.beast_helper.utils import change_taxon_map
 def test_change_taxon_map(ten_taxa_multichain):
     in_nexus = os.path.join(ten_taxa_multichain.working_dir, ten_taxa_multichain.tree_files[0])
     out_nexus = os.path.join(ten_taxa_multichain.working_dir, f"changed_mapping_{ten_taxa_multichain.tree_files[0]}")
-    new_map = { 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10}
+    new_map = {i: f"{i}" for i in range(1, 11)}
     change_taxon_map(input_nexus=in_nexus, output_nexus=out_nexus, new_map=new_map)
     assert True
 
@@ -14,6 +14,6 @@ def test_change_taxon_map(ten_taxa_multichain):
 def test_change_taxon_map_mcc(ten_taxa_multichain):
     in_nexus = os.path.join(ten_taxa_multichain.working_dir, f"chain0_1-mcc.tree")
     out_nexus = os.path.join(ten_taxa_multichain.working_dir, f"changed_mapping_mcc.tree")
-    new_map = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10}
+    new_map = {i: f"{i}" for i in range(1, 11)}
     change_taxon_map(input_nexus=in_nexus, output_nexus=out_nexus, new_map=new_map)
     assert True
