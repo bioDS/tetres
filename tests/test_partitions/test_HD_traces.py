3a795fd0412fa5395693e9c9171b3356c80eca82
Lars-B <berlinglars96@gmail.com>
2023-09-22 08:00:05 +1200

    entropy dissonance trace data for CPD

--- tests/test_partitions/test_HD_traces.py
+++ tests/test_partitions/test_HD_traces.py
@@ -1,7 +1,10 @@
 from tetres.partitions.HD_traces import *
 
 
-def test_bla(ten_taxa_multichain):
+def test_hd_trace(ten_taxa_multichain):
     data = entropy_trace_data(ten_taxa_multichain)
     assert True
 
+def test_hd_ranked_trace(ten_taxa_multichain):
+    data = entropy_trace_ranked_data(ten_taxa_multichain)
+    assert True
