b93d3ee4031fe1ada297e10392531f9b5d60ed2c
Lars-B <berlinglars96@gmail.com>
2023-09-26 09:23:54 +1300

    add argument to exclude return of ranked stuff

--- tests/test_partitions/test_estimatedP_vs_markovP.py
+++ tests/test_partitions/test_estimatedP_vs_markovP.py
@@ -3,3 +3,8 @@ from tetres.partitions.estimatedP_vs_markovP import *
 def test_ccd_cpd_vs_markov(twenty_taxa_tts):
     ud, rd = ccd_cpd_vs_markov(twenty_taxa_tts)
     assert (len(ud), len(rd)) == (1219, 2499), "CCD, CPD data went wrong."
+
+
+def test_ccd_cpd_vs_markov_norank(twenty_taxa_tts):
+    ud = ccd_cpd_vs_markov(twenty_taxa_tts, include_rank=False)
+    assert len(ud) == 1219, "CCD, CPD data went wrong."
