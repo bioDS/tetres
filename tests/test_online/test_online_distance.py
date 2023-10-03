221571e0bdfbc888e5bbdaaf6217ff529bdcade8
Lars-B <berlinglars96@gmail.com>
2023-10-03 15:26:24 +1300

    update partitions, new test for online stuff

--- tests/test_online/test_online_distance.py
+++ tests/test_online/test_online_distance.py
@@ -1,7 +1,12 @@
 
-from tetres.online.online_distance import online_distance_neighbourhood
+from tetres.online.online_distance import online_distance_neighbourhood, new_function
 
 
 def test_online_distance_neighbourhood(twelve_taxa_tts):
-    assert online_distance_neighbourhood(twelve_taxa_tts[0], twelve_taxa_tts[1]) == 0,\
-        "Online distance neighbourhood failed!"
+    d = online_distance_neighbourhood(twelve_taxa_tts[0], twelve_taxa_tts[1])
+    assert True
+
+
+def test_new_c_stuff(twelve_taxa_tts):
+    res = new_function(twelve_taxa_tts)
+    assert res == 1, "New Stuff failed"
