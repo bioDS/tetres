b3d204655cd30dfb88c9758b450891700871dd4d
Lars-B <berlinglars96@gmail.com>
2023-10-08 15:10:28 +1300

    adding NoBetterNeighbourFound exception throwing, returning -1,-1 in c and test for this.

--- tests/test_online/test_online_distance.py
+++ tests/test_online/test_online_distance.py
@@ -1,6 +1,10 @@
 from tetres.online.online_distance import greedy_online_omp, search_neighbourhood_greedy_online_omp,\
-    intelligent_neighbourhood_online, get_precomp_list, free_precomp_list
+    intelligent_neighbourhood_online, get_precomp_list, free_precomp_list, _compute_and_free_precomp_list
+from tetres.summary._tree_proposals import search_neighbourhood_greedy, NoBetterNeighbourFound
+from tetres.summary.compute_sos import compute_sos_mt
+from tetres.trees.time_trees import TimeTree
 
+import pytest
 import psutil
 import gc
 
@@ -25,6 +29,18 @@ def test_intelligent_neighbourhood_online_twenty(twenty_taxa_tts):
     assert after - before < 10.0, "Failed Memory usage test (20 taxa)"
 
 
+def test_suspicious_memory(twelve_taxa_tts, twelve_taxa_tts_start):
+    import tracemalloc
+    before = psutil.Process().memory_info().rss / 1024 ** 2  # in MiB
+    tracemalloc.start()
+    s0 = tracemalloc.take_snapshot()
+    for _ in range(10):
+        _compute_and_free_precomp_list(twelve_taxa_tts, twelve_taxa_tts_start[0])
+    after = psutil.Process().memory_info().rss / 1024 ** 2  # in MiB
+    s1 = tracemalloc.take_snapshot()
+    assert after-before < 10.0, "Failed memory usage test"
+
+
 def test_get_precomp_list_twelve_memory(twelve_taxa_tts, twelve_taxa_tts_start):
     before = psutil.Process().memory_info().rss / 1024 ** 2  # in MiB
     for _ in range(100):
@@ -45,10 +61,30 @@ def test_get_precomp_list_twenty(twenty_taxa_tts, twenty_taxa_tts_start):
     assert after - before < 10.0, "get_precomp_list failed Memory usage Test (12 taxa)"
 
 
-def test_search_neighbourhood_greedy_online_omp_twelve(twelve_taxa_tts, twelve_taxa_tts_start):
-    search_neighbourhood_greedy_online_omp(twelve_taxa_tts_start, twelve_taxa_tts)
+def test_memory_search_neighbourhood_greedy_online_omp_twelve(twelve_taxa_tts, twelve_taxa_tts_start):
+    precomp_list = get_precomp_list(twelve_taxa_tts, twelve_taxa_tts_start[0])
+    before = psutil.Process().memory_info().rss / 1024 ** 2  # in MiB
+    centroid, sos = search_neighbourhood_greedy_online_omp(twelve_taxa_tts_start[0], twelve_taxa_tts, precomp_list)
+    after = psutil.Process().memory_info().rss / 1024 ** 2  # in MiB
+    assert after-before < 10.0, "search_neighbourhood_greedy_online_omp failed Memory usage! There is a memory leak."
 
-    assert True
+
+def test_functionality_search_neighbourhood_greedy_online_omp_twelve(twelve_taxa_tts, twelve_taxa_tts_start):
+    precomp_list = get_precomp_list(twelve_taxa_tts, twelve_taxa_tts_start[0])
+    best_neigh_online, sos_online = search_neighbourhood_greedy_online_omp(twelve_taxa_tts_start[0], twelve_taxa_tts, precomp_list)
+    start_sos = compute_sos_mt(twelve_taxa_tts_start[0], twelve_taxa_tts)
+    best_neigh_offline, sos_offline = search_neighbourhood_greedy(twelve_taxa_tts_start[0], twelve_taxa_tts, t_value=start_sos)
+    assert all([sos_online == sos_offline, best_neigh_online.fp_distance(best_neigh_offline) == 0]),\
+        "Failed to find the same best neighbour using the online distance for greedy neighbourhood search!"
+
+
+# todo test what if we do not have a better neighbour, implement throwing the exception
+def test_nobetterneighbour_search_neighbourhood_greedy_online_omp_twelve(five_taxa_tts):
+    with pytest.raises(NoBetterNeighbourFound):
+        # This Error is raised because of t_value being to low!
+        other = TimeTree('((1:3,5:3):1,(3:2,(4:1,2:1):1):2);')
+        precomp_list = get_precomp_list(five_taxa_tts, other)
+        search_neighbourhood_greedy_online_omp(other, five_taxa_tts, precomp_list)
 
 
 def test_search_neighbourhood_greedy_online_omp_twenty(twelve_taxa_tts, twenty_taxa_tts_start):
