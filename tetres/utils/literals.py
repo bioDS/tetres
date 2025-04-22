f60021e53c6b69cd997b0bb07969cdeddd49a6e2
Lars-B <berlinglars96@gmail.com>
2025-04-22 10:37:56 -0700

    WIP: adding clustering type literal

--- tetres/utils/literals.py
+++ tetres/utils/literals.py
@@ -3,6 +3,6 @@ Module contains Literals that are used for function input across the package.
 """
 from typing import Literal
 
-_MDS_TYPES = Literal["tsne"]
-# TODO the dist is more a core thing that should be within a different module...
 _DIST = Literal["rnni", "rf"]
+_MDS_TYPES = Literal["tsne"]
+_CLUSTERING_TYPE = Literal["spectral"]
